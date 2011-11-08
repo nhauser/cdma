307303752de9aab536403e15c9a3a13632b6be51
calltl <calltl@gmail.com>
2011-11-08 23:22:59 +0000

    First CDMA code commit

---
+++ java/trunk/org.gumtree.data.core/src/org/gumtree/data/interfaces/IKey.java
@@ -0,0 +1,76 @@
+package org.gumtree.data.interfaces;
+
+import java.util.List;
+
+import org.gumtree.data.dictionary.IPathParameter;
+
+/**
+ * The IKey is used by group to interrogate the dictionary. Indeed the key's 
+ * name corresponds to an entry in the dictionary. This entry targets a path in the
+ * currently explored document. The group will open it.
+ * <p>
+ * The IKey can carry some filters to help group to decide which node is relevant.
+ * The filters can specify an order index to open a particular type of node, an 
+ * attribute, a part of the name... 
+ * <p>
+ * @author rodriguez
+ */
+
+public interface IKey extends IModelObject, Cloneable, Comparable<Object> {
+    /**
+     * Get the entry name in the dictionary that will be 
+     * searched when using this key. 
+     * 
+     * @return the name of this key
+     */
+    public String getName();
+    
+    /**
+     * Set the entry name in the dictionary that will be 
+     * searched when using this key. 
+     * 
+     * @param name of this key
+     */
+    public void setName(String name);
+    
+    /**
+     * Return true if both key have similar names.
+     * Filters are not compared. 
+     * @param key to compare
+     * @return true if both keys have same name
+     */
+    public boolean equals(IKey key);
+    
+    /**
+     * Get the list of parameters that will be applied when using this key.
+     * 
+     * @return list of IPathParameter 
+     */
+    public List<IPathParameter> getParameterList();
+    
+    /**
+     * Add a IPathParameter to this IKey that will be used when 
+     * searching an object with this key. .
+     * 
+     * @param parameter to be applied
+     * @note work as a FILO
+     */
+    public void pushParameter(IPathParameter filter);
+    
+    /**
+     * Remove a IPathParameter to this IKey that will be used when 
+     * searching an object with this key.
+     * 
+     * @return parameter that won't be applied anymore
+     * @note work as a FILO
+     */
+    public IPathParameter popParameter();
+    
+    public String toString();
+    
+    /**
+     * Copy entirely the key : name and filters are cloned
+     * @return a copy of this key
+     */
+    public IKey clone();
+}
