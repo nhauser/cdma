8539a526e3e982397317adbe14caa76120e061ec
clem.rodrig <clem.rodrig@gmail.com>
2011-11-15 16:04:29 +0000

    SOLEIL release version of NexusSoleil plugin 1.3.0 (SOLEIL only)

---
+++ java/tags/NexusSoleil-1.3.0/src/org/gumtree/data/soleil/internal/DictionaryDetector.java
@@ -0,0 +1,170 @@
+package org.gumtree.data.soleil.internal;
+
+import org.gumtree.data.exception.FileAccessException;
+import org.gumtree.data.soleil.navigation.NxsDataset;
+import org.nexusformat.NexusException;
+
+import fr.soleil.nexus4tango.NexusFileWriter;
+import fr.soleil.nexus4tango.NexusNode;
+import fr.soleil.nexus4tango.PathGroup;
+import fr.soleil.nexus4tango.PathNexus;
+
+public class DictionaryDetector {
+	final static String SEPARATOR = "_";
+	final static String EXTENSION = ".xml";
+	private NxsDataset m_dataset;
+	private Beamline   m_beamline;
+	private DataModel  m_model;
+	
+	public DictionaryDetector(NxsDataset dataset) {
+		m_dataset = dataset;
+	}
+
+	public String getDictionaryName() throws FileAccessException {
+		String fileName = null;
+		if( m_beamline == null ) {
+			detectBeamline();
+		}
+		if( m_model == null ) {
+			detectDataModel();
+		}
+		
+		fileName = m_beamline.getName() + SEPARATOR + m_model.getName() + EXTENSION;
+		return fileName;
+	}
+	
+	public Beamline detectBeamline() {
+		m_beamline = null;
+		PathNexus path = new PathGroup(new String[] {"<NXentry>", "<NXinstrument>"});
+		NexusFileWriter handler = m_dataset.getHandler();
+	
+		try {
+			handler.openPath(path);
+			m_beamline = Beamline.valueOf(handler.getCurrentPath().getCurrentNode().getNodeName().toUpperCase());
+		} catch (Exception e) {
+			m_beamline = Beamline.UNKNOWN;
+		}
+		
+		try {
+			handler.closeAll();
+		} catch (NexusException e) {}
+		
+		return m_beamline;
+	}
+	
+	public DataModel detectDataModel() {
+		if( m_beamline == null ) {
+			detectBeamline();
+		}
+		
+		switch( m_beamline ) {
+			case ANTARES:
+				m_model = detectDataModelAntares();
+				break;
+			case SWING:
+				m_model = detectDataModelSwing();
+				break;
+			case CONTACQ:
+				m_model = detectDataModelICA();
+				break;
+			case METROLOGIE:
+				m_model = detectDataModelMetrologie();
+				break;
+			case DISCO:
+				m_model = detectDataModelDisco();
+				break;
+			case UNKNOWN:
+			default:
+				m_model = DataModel.UNKNOWN;
+				break;
+		}
+		return m_model;
+	}
+	
+	
+	// ------------------------------------------------------------------------------------------------------------------
+    // ------------------------------------------------------------------------------------------------------------------
+    /// protected methods: to guess the data model associated to a specific beamline
+    // ------------------------------------------------------------------------------------------------------------------
+    // ------------------------------------------------------------------------------------------------------------------
+	protected DataModel detectDataModelAntares() {
+		return detectStandardDataModel();
+	}
+	
+	protected DataModel detectDataModelSwing() {
+		return detectStandardDataModel();
+	}
+	
+	protected DataModel detectDataModelICA() {
+		return detectStandardDataModel();
+	}
+	
+	protected DataModel detectDataModelMetrologie() {
+		return detectStandardDataModel();
+	}
+	
+	protected DataModel detectDataModelDisco() {
+		return detectStandardDataModel();
+	}
+
+	// ------------------------------------------------------------------------------------------------------------------
+    // ------------------------------------------------------------------------------------------------------------------
+    /// protected methods: to guess the data model in general way
+    // ------------------------------------------------------------------------------------------------------------------
+    // ------------------------------------------------------------------------------------------------------------------
+	protected boolean isScanServer() {
+		boolean result = false;
+		NexusFileWriter handler = m_dataset.getHandler();
+		try {
+			PathNexus path = new PathGroup(new String[] {"<NXentry>", "scan_data<NXdata>"});
+			handler.openPath(path);
+			handler.openNode( new NexusNode("time_1", "SDS") );
+			result = true;
+			handler.closeAll();
+		}
+		catch (NexusException e) { }
+		
+		try {
+			handler.closeAll();
+		} catch (NexusException e) { }
+		
+		return result;
+	}
+	
+	protected boolean isFlyScan() {
+		boolean result = false;
+		NexusFileWriter handler = m_dataset.getHandler();
+		try {
+			PathNexus path = new PathGroup(new String[] {"<NXentry>", "scan_data<NXdata>"});
+			handler.openPath(path);
+			try {
+				handler.openNode( new NexusNode("time_1", "SDS") );
+			} catch (NexusException e) {
+				result = true;
+			}
+		}
+		catch (NexusException e) { }
+		
+		try {
+			handler.closeAll();
+		} catch (NexusException e) { }
+		
+		return result;
+	}
+	
+	protected DataModel detectStandardDataModel() {
+		DataModel model = DataModel.UNKNOWN;
+		
+		if( isScanServer() ) {
+			model = DataModel.SCANSERVER;
+		}
+		else if( isFlyScan() ) {
+			model = DataModel.FLYSCAN;
+		}
+		else {
+			model = DataModel.PASSERELLE;
+		}
+		
+		return model;
+	}
+}
