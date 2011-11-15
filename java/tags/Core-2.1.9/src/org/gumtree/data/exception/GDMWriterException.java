228a05c8510e45e1df4038a41d04cdc7d93c195d
clem.rodrig <clem.rodrig@gmail.com>
2011-11-15 16:01:15 +0000

    SOLEIL release version 2.1.9 (SOLEIL only)

---
+++ java/tags/Core-2.1.9/src/org/gumtree/data/exception/GDMWriterException.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Australian Nuclear Science and Technology Organisation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ *    Norman Xiong (nxi@Bragg Institute) - initial API and implementation
+ ******************************************************************************/
+package org.gumtree.data.exception;
+
+/**
+ * @author nxi
+ * 
+ */
+public class GDMWriterException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6935950511233199000L;
+
+	/**
+		 * 
+		 */
+	public GDMWriterException() {
+		super();
+	}
+
+	/**
+	 * @param arg0
+	 *            String value
+	 */
+	public GDMWriterException(final String arg0) {
+		super(arg0);
+	}
+
+	/**
+	 * @param arg0
+	 *            Throwable object
+	 */
+	public GDMWriterException(final Throwable arg0) {
+		super(arg0);
+	}
+
+	/**
+	 * @param arg0
+	 *            String value
+	 * @param arg1
+	 *            Throwable object
+	 */
+	public GDMWriterException(final String arg0, final Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+}
