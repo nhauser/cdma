8f3d573378fb9386af67feeb3ab30790f8a8b338
clem.rodrig <clem.rodrig@gmail.com>
2011-12-14 12:39:53 +0000

    SOLEIL Bug fix

--- java/branches/soleil/org.gumtree.data.engine.jnexus/src/org/gumtree/data/engine/jnexus/array/NexusArray.java
+++ java/branches/soleil/org.gumtree.data.engine.jnexus/src/org/gumtree/data/engine/jnexus/array/NexusArray.java
@@ -1,8 +1,6 @@
 package org.gumtree.data.engine.jnexus.array;
 
 import org.gumtree.data.engine.jnexus.NexusFactory;
-import org.gumtree.data.engine.jnexus.utils.NexusArrayMath;
-import org.gumtree.data.engine.jnexus.utils.NexusArrayUtils;
 import org.gumtree.data.exception.BackupException;
 import org.gumtree.data.exception.InvalidRangeException;
 import org.gumtree.data.exception.ShapeNotMatchException;
@@ -77,12 +75,12 @@ public final class NexusArray implements IArray {
 	// ---------------------------------------------------------
     @Override
     public IArrayMath getArrayMath() {
-        return new NexusArrayMath(this);
+        return NexusFactory.createArrayMath(this);
     }
 
     @Override
     public IArrayUtils getArrayUtils() {
-        return new NexusArrayUtils(this);
+    	return NexusFactory.createArrayUtils(this);
     }
     
 	/// Specific method to match NetCDF plug-in behavior
@@ -333,7 +331,7 @@ public final class NexusArray implements IArray {
 		NexusArray result = new NexusArray(this);
 		
 		if( data ) {
-			result.mData = NexusArrayUtils.copyJavaArray(mData);
+			result.mData = copyJavaArray(mData);
 		}
 		
 		return result;
@@ -348,6 +346,64 @@ public final class NexusArray implements IArray {
     public String getFactoryName() {
     	return NexusFactory.NAME;
     }
+    
+	// --------------------------------------------------
+	// tool methods
+	// --------------------------------------------------
+    static public Object copyJavaArray(Object array) {
+		Object result = array;
+		if( result == null ) {
+			return null;
+		}
+		else {
+			// Determine rank of array (by parsing data array class name)
+			String sClassName = array.getClass().getName();
+			int iRank  = 0;
+			int iIndex = 0;
+			char cChar;
+			while (iIndex < sClassName.length()) {
+				cChar = sClassName.charAt(iIndex);
+				iIndex++;
+				if (cChar == '[') {
+					iRank++;
+				}
+			}
+	
+			// Set dimension rank
+			int[] shape    = new int[iRank];
+	
+			// Fill dimension size array
+			for ( int i = 0; i < iRank; i++) {
+				shape[i] = java.lang.reflect.Array.getLength(result);
+				result = java.lang.reflect.Array.get(result,0);
+			}
+			
+			// Define a convenient array (shape and type)
+			result = java.lang.reflect.Array.newInstance(array.getClass().getComponentType(), shape);
+			result = copyJavaArray(array, result);
+		}
+		return result;
+	}
+	
+	static public Object copyJavaArray(Object source, Object target) {
+		Object item = java.lang.reflect.Array.get(source, 0);
+		int length = java.lang.reflect.Array.getLength(source);
+
+		if( item.getClass().isArray() ) {
+			Object tmpSrc;
+			Object tmpTar;
+			for( int i = 0; i < length; i++ ) {
+				tmpSrc = java.lang.reflect.Array.get(source, i);
+				tmpTar = java.lang.reflect.Array.get(target, i);
+				copyJavaArray( tmpSrc, tmpTar);
+			}
+		}
+		else {
+			System.arraycopy(source, 0, target, 0, length);
+		}
+		
+		return target;
+	}
     
     // ---------------------------------------------------------
 	/// Protected methods
