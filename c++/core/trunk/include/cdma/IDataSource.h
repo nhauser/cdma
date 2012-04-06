a20687a3aacd7e8d7dd5169ab2926a192f9416e0
stephane.poirier <stephane.poirier@synchrotron-soleil.fr>
2012-04-06 13:30:50 +0000

    Updated description

--- c++/core/trunk/include/cdma/IDataSource.h
+++ c++/core/trunk/include/cdma/IDataSource.h
@@ -30,7 +30,7 @@ namespace cdma
 {
 
 //==============================================================================
-/// @brief Abstraction of the plugin dataset recognition
+/// @brief Abstraction of the plugin data source information
 ///
 /// The CDMA asks each plug-ins through this interface to find out which one
 /// is the best choice for accessing the data referenced by the given URI
@@ -39,42 +39,47 @@ class CDMA_DECL IDataSource
 {
 public:
 
-  /// Ask the plugin if the URI corresponds to a strictly readable dataset
-  /// Only the basic navigation is applicable in such a dataset
-  /// The dictionnary mechanism is not relevant for this kind of dataset
+  /// Ask the plugin if the location corresponds to a readable dataset
+  /// using at least the standard navigation interface.
+  ///
+  /// It's a necessary condition for reading the dataset but not
+  /// sufficient for decide to apply the dictionary on it.
+  ///
+  /// @see isExperiment
   ///
   /// @param dataset_location location of the dataset asked for
-  ///
   /// @return true of false
   ///
   virtual bool isReadable(const yat::URI& dataset_location) const = 0;
   
-  /// Ask the plugin if the URI corresponds to a strictly readable dataset
-  /// Only the basic navigation is applicable in such a dataset
-  /// The dictionnary mechanism is not relevant for this kind of dataset
+  /// When browsing a file system, ask the plugin if the location corresponds
+  /// to a file containing one or more datasets, at this plug-in sense.
+  ///
+  /// If the answer is true, the client should open the file (through 
+  /// a yet to be developed CDMA service) the same way the directory
+  /// containing it.
   ///
   /// @param dataset_location location of the dataset asked for
-  ///
-  /// @return true of false
+  /// @return true or false
   ///
   virtual bool isBrowsable(const yat::URI& dataset_location) const = 0;
 
-  /// Ask the plugin if the URI corresponds to a strictly readable dataset
-  /// Only the basic navigation is applicable in such a dataset
-  /// The dictionnary mechanism is not relevant for this kind of dataset
+  /// Ask the plug-in if the location corresponds to a file produced by the 
+  /// institute's editor of this plug-in.
+  ///
+  /// It make sense for a dataset that is a directory containing several
+  /// files: each file is effectively produced by the institute who provide 
+  /// the plug-in, but is not a dataset, just a part.
   ///
   /// @param dataset_location location of the dataset asked for
-  ///
   /// @return true of false
   ///
   virtual bool isProducer(const yat::URI& dataset_location) const = 0;
   
-  /// Ask the plugin if the URI corresponds to a strictly readable dataset
-  /// Only the basic navigation is applicable in such a dataset
-  /// The dictionnary mechanism is not relevant for this kind of dataset
+  /// Ask the plugin if the location corresponds to a dataset produced
+  /// by the same institute providing this plug-in.
   ///
   /// @param dataset_location location of the dataset asked for
-  ///
   /// @return true of false
   ///
   virtual bool isExperiment(const yat::URI& dataset_location) const = 0;
