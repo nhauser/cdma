3ca48b31525579d651000ff8828085134fc8a6f7
clem.rodrig <clem.rodrig@gmail.com>
2012-06-27 14:19:14 +0000

    [SOLEIL][JAVA] Soleil's branch:
    CDMA Core API:
    - packages renamed org.gumtree.data to org.cdma
    - added synonyms dictionary mechanism
    - removed all Extended Dictionary mechanism (EDM) interfaces (a standard mechanism is provided with the Core)
    - removed PathParameter
    - added IFilter to select nodes no more using divination and obscures processes but with their properties (CDMA ones)
    - external code in EDM loading uses Java Service Loader or OSGI Bundle Service
    - external code in EDM changed, no more specific deployment is needed: plug-in is scanned for IPluginMethod implementation
    - external code execution is standardized using IPluginMethod interface
    
    CDMA Nexus Engine:
    - packages renamed org.gumtree.data to org.cdma
    - reading data optimizations
    - bug fixed in the navigation cache system
    - bug fix in array caching system
    
    CDMA Soleil Plugin:
    - packages renamed org.gumtree.data to org.cdma
    - factory name changed to fit Cpp needs and allow compatibility with dictionary's folder structure
    - plug-in refactored to fit core's requirements (IPluginMethod)

---
+++ java/branches/soleil/org.gumtree.data.core/src/org/cdma/interfaces/IContainer.java
@@ -0,0 +1,176 @@
+// ****************************************************************************
+// Copyright (c) 2008 Australian Nuclear Science and Technology Organisation.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0 
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors: 
+//    Norman Xiong (nxi@Bragg Institute) - initial API and implementation
+//    Clément Rodriguez (clement.rodriguez@synchrotron-soleil.fr) - API evolution
+// ****************************************************************************
+package org.cdma.interfaces;
+
+import java.util.List;
+
+import org.cdma.internal.IModelObject;
+import org.cdma.utils.Utilities.ModelType;
+
+/**
+ * @brief The IContainer interface is shared IGroup and IDataItem.
+ */
+
+/// @cond pluginAPI
+
+/**
+ * @note When developing a plug-in consider using the data format engine's implementation. 
+ * You should <b>redefine this interface implementation</b>, only in case of <b>very specific needs.</b>
+ * <p>
+ */
+
+/// @endcond pluginAPI
+
+/// @cond engineAPI
+
+/**
+ * @note When developing a data format engine do not implement this interface. It is inherited yet by IDataItem and IGroup interfaces. 
+ * <p>
+ */
+
+/// @endcond engineAPI
+
+/**
+ * The IContainer contains behaviors a node, in a tree of data, should
+ * respect.
+ * 
+ * @author nxi
+ * 
+ */
+public interface IContainer extends IModelObject, Cloneable {
+
+    /**
+     * Get the ModelType implemented by this object.
+     * 
+     * @return ModelType of this object
+     */
+    ModelType getModelType();
+
+    /**
+     * Add an Attribute to the Group.
+     * 
+     * @param attribute CDMA Attribute 
+     */
+    void addOneAttribute(IAttribute attribute);
+
+    /**
+     * A convenience method of adding a String type attribute.
+     * 
+     * @param name String type object
+     * @param value String type object 
+     */
+    void addStringAttribute(String name, String value);
+
+    /**
+     * Find an Attribute in this Group by its name.
+     * 
+     * @param name the name of the attribute
+     * @return the attribute, or null if not found
+     */
+    IAttribute getAttribute(String name);
+
+    /**
+     * Get the set of attributes contained directly in this Group.
+     * 
+     * @return List of type Attribute; may be empty, not null.
+     */
+    List<IAttribute> getAttributeList();
+
+    /**
+     * Get the Dataset that hold the current Group.
+     * 
+     * @return CDMA Dataset 
+     */
+    IDataset getDataset();
+
+    /**
+     * Get the location referenced by the Dataset.
+     * 
+     * @return String type 
+     */
+    String getLocation();
+
+    /**
+     * Get the (long) name of the IObject, which contains the path information.
+     * 
+     * @return String type object 
+     */
+    String getName();
+
+    /**
+     * Get its parent Group, or null if its the root group.
+     * 
+     * @return CDMA group object
+     */
+    IContainer getParentGroup();
+
+    /**
+     * Get the root group of the tree that holds the current Group.
+     * 
+     * @return CDMA Group 
+     */
+    IContainer getRootGroup();
+
+    /**
+     * Get the "short" name, unique within its parent Group.
+     * 
+     * @return String object
+     */
+    String getShortName();
+
+    /**
+     * Check if the Group has an Attribute with certain name and value.
+     * 
+     * @param name in String type
+     * @param value in String type
+     * @return boolean type 
+     */
+    boolean hasAttribute(String name, String value);
+
+    /**
+     * Remove an Attribute from the Attribute list.
+     * 
+     * @param attribute CDMA Attribute
+     * @return boolean type 
+     */
+    boolean removeAttribute(IAttribute attribute);
+
+    /**
+     * Set the IObject's (long) name.
+     * 
+     * @param name String object
+     */
+    void setName(String name);
+
+    /**
+     * Set the IObject's (short) name.
+     * 
+     * @param name in String type 
+     */
+    void setShortName(String name);
+
+    /**
+     * Set the parent group.
+     * 
+     * @param group IGroup object
+     */
+    void setParent(IGroup group);
+
+
+    /**
+     * Clone this IContainer.
+     * 
+     * @return new IDataItem instance
+     */
+    IContainer clone() throws CloneNotSupportedException;
+
+}
---
+++ java/branches/soleil/org.gumtree.data.core/src/org/cdma/internal/dictionary/ItemSolver.java
@@ -0,0 +1,196 @@
+// ****************************************************************************
+// Copyright (c) 2008 Australian Nuclear Science and Technology Organisation.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0 
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors: 
+//    Clément Rodriguez - initial API and implementation
+// ****************************************************************************
+package org.cdma.internal.dictionary;
+
+/// @cond internal
+
+/**
+ * @brief ItemSolver aims to return an IContainer that corresponds to the mapping.
+ * 
+ * The ItemSolver class as an unique method which solve. The goal is to return
+ * the IContainer that is defined into the institute's mapping. To do so, it uses
+ * a Context which permits to access the CDMA environment. The IContainer can be 
+ * searched in a specific path, or might need a named method execution a to construct it.
+ * <p>
+ * That class is used internally by the LogicalGroup to construct item.
+ * 
+ * @see org.cdma.dictionary.IPluginMethod
+ * @see org.cdma.dictionary.Context
+ * 
+ * @author rodriguez
+ *
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.cdma.IFactory;
+import org.cdma.dictionary.Context;
+import org.cdma.dictionary.IPluginMethod;
+import org.cdma.dictionary.filter.IFilter;
+import org.cdma.interfaces.IAttribute;
+import org.cdma.interfaces.IContainer;
+import org.cdma.interfaces.IKey;
+import org.jdom2.Element;
+
+public class ItemSolver {
+    IFactory               mFactory;      // IFactory instance of the plug-in using this item solver
+    List<Solver>           mContent;      // List of solvers to process to get IContainer content
+    List<AttributeSolver > mAttributes;   // List of attribute solvers to process to get IContainer attributes
+    
+    public ItemSolver(IFactory factory, PluginMethodManager manager, Element elem) {
+        mFactory = factory;
+
+        // Prepare list of solver
+        mContent = new ArrayList<Solver>();
+        
+        // Prepare list of attribute solvers
+        mAttributes = new ArrayList<AttributeSolver>();
+        
+        // Initialize internal fields
+        init(manager, elem);
+    }
+    
+    public ItemSolver(IFactory factory, IKey key) {
+        mFactory = factory;
+
+        // Prepare list of solver
+        mContent = new ArrayList<Solver>();
+        
+        // Prepare list of attribute solvers
+        mAttributes = new ArrayList<AttributeSolver>();
+        
+        // Initialize internal fields
+        mContent.add( new Solver(key) );
+    }
+    
+    public List<IContainer> solve(Context context) {
+        List<IContainer> valid = new ArrayList<IContainer>();
+        List<IContainer> found = new ArrayList<IContainer>();
+        
+        // Get the key from the context
+        IKey key = context.getKey();
+
+        // Execute sequentially each solver
+        for( Solver solver : mContent ) {
+            found = solver.solve(context);
+            
+            // Check found items match what is requested by key
+            valid.clear();
+            for( IContainer container : found ) {
+                if( isValidContainer( key, container ) ) {
+                    valid.add(container);
+                }
+            }
+            // Update the context with the last found item
+            context.setContainers(valid);
+        }
+
+        // Add attributes defined in the solver 
+        if( ! mAttributes.isEmpty() ) {
+            context.setContainers(new ArrayList<IContainer>());
+            IAttribute attrib;
+            
+            // For each valid container
+            for( IContainer container : valid ) {
+                // For each attributes to create
+                for( AttributeSolver solver : mAttributes ) {
+                    
+                    // Update the context with container to process as a parameter 
+                    //context.setParams( new IContainer[] {container});
+                    
+                    // Resolve the attribute
+                    attrib = solver.solve(context);
+                    
+                    // Add the attribute to the IContainer
+                    if( attrib != null ) {
+                        container.addOneAttribute(attrib);
+                    }
+                }
+            }
+        }
+
+        return valid;
+    }
+    
+    
+    // ---------------------------------------------------------
+    /// Private methods
+    // ---------------------------------------------------------
+    @SuppressWarnings("unchecked")
+    private void init(PluginMethodManager manager, Element elem) {
+        // Temporary variables
+        IPluginMethod method;
+        Solver current;
+        
+        // List DOM children 
+        List<?> nodes = elem.getChildren();
+        List<?> attrs;
+                
+        // For each children of the mapping key item
+        for( Element node : (List<Element>) nodes ) {
+            // If path open the path
+            if( node.getName().equals("path") )  {
+                current = new Solver( mFactory.createPath(node.getText()) );
+                mContent.add(current);
+            }
+            // If call on a method
+            else if( node.getName().equals("call") )  {
+                method = manager.getPluginMethod(mFactory.getName(), node.getText());
+                current = new Solver(method);
+                mContent.add( current );
+            }
+            // If attribute
+            else if( node.getName().equals("attribute") ) {
+                // For each children of the mapping attribute
+                attrs = node.getChildren();
+                String attrName = node.getAttributeValue("name");
+                // Create a list of solver for that particular attribute
+                List<Solver> attrSolv = new ArrayList<Solver>();
+                for( Element subNode : (List<Element>) attrs ) {
+                    // If path open the path
+                    if( subNode.getName().equals("path") )  {
+                        current = new Solver( mFactory.createPath(subNode.getText()) );
+                        attrSolv.add(current);
+                    }
+                    // If call on a method
+                    else if( subNode.getName().equals("call") )  {
+                        method = manager.getPluginMethod(mFactory.getName(), subNode.getText());
+                        current = new Solver(method);
+                        attrSolv.add( current );
+                    }
+                }
+                // Store the attribute into a list
+                mAttributes.add( new AttributeSolver(mFactory, attrName, attrSolv) );
+            }
+        }
+    }
+    
+    /**
+     * Check if the IContainer is conform to the IKey's filters
+     * @param key IKey with filters if any
+     * @param container IContainer to check
+     * @return true if the IContainer is compliant with the IKey
+     */
+    private boolean isValidContainer(IKey key, IContainer container) {
+        boolean result = true;
+        for( IFilter filter : key.getFilterList() ) {
+            if( ! filter.matches(container) ) {
+                result = false;
+                break;
+            }
+        }
+        
+        return result;
+    }
+}
+
+/// @endcond internal
