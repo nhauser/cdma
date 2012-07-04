401c320fb0ed9d710cbaeb8280d88f64b312a1a7
eugen.wintersberger <eugen.wintersberger@gmail.com>
2012-07-04 09:38:21 +0000

    Added license header to all source files in ./src

--- bindings/python/trunk/src/DimensionWrapper.hpp
+++ bindings/python/trunk/src/DimensionWrapper.hpp
@@ -1,3 +1,26 @@
+/*
+ * (c) Copyright 2012 DESY, Eugen Wintersberger <eugen.wintersberger@desy.de>
+ *
+ * This file is part of cdma-python.
+ *
+ * cdma-python is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * cdma-python is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cmda-python.  If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************
+ *
+ * Created on: Jul 04, 2011
+ *     Author: Eugen Wintersberger <eugen.wintersberger@desy.de>
+ */
+
 #ifndef __DIMENSIONWRAPPER_HPP__
 #define __DIMENSIONWRAPPER_HPP__
 
