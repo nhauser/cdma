6151e8577e0ba3f830b9132e2b4dfcb714643632
eugen.wintersberger@gmail.com <eugen.wintersberger@gmail.com@969d0bd1-f5ba-ef64-2eb3-b0521df2be17>
2012-07-06 14:33:29 +0000

    Changed demo due to new interfaces.

--- bindings/python/trunk/test/demo.py
+++ bindings/python/trunk/test/demo.py
@@ -2,34 +2,19 @@ from matplotlib import pyplot
 import numpy
 import cdma
 
-def print_attribute(attr):
-    ostr = "attribute ["+attr.name + "]:"
-    ostr += " size=%i," %(attr.size)
-    ostr += " type=%s," %(attr.type)
-    ostr += " shape=%s," %(attr.shape.__str__())
-    ostr += " value=%s" %(attr[...].__str__())
-
-    print ostr 
-
-def print_item(item):
-    ostr = "dataitem ["+item.name+"]:"
-    ostr += " type=%s," %(item.type)
-    ostr += " rank=%i," %(item.rank)
-    ostr += " shape=%s," %(item.shape.__str__())
-    ostr += "\ndata = \n%s" %(item[...].__str__())
-
-    print ostr
 
 dataset = cdma.open_dataset("file:../data/demo.nxs")
+rg = dataset.root_group
 
 print "scan group..."
-scan_group = dataset["D1A_016_D1A"]
+scan_group = rg["D1A_016_D1A"]
 for i in scan_group.items:
     print i.name,i.type,i.size
 
-print_attribute(scan_group.attrs["name"])
+print scan_group.attrs["name"]
 
-print scan_group.dims
+print scan_group["start_time"][...]
+print scan_group["end_time"][...]
 
 print "read data ..."
 #print scan_group["duration"][...]
@@ -39,8 +24,7 @@ image_group = scan_group["image#20"]
 
 print "data item ..."
 data= image_group["data"]
-print_item(data)
-#print data[0:100:2,50:1000:20]
+print data
 
 for a in data.attrs:
     print a
@@ -48,7 +32,7 @@ for a in data.attrs:
 pyplot.figure()
 pyplot.imshow(numpy.log10(data[...]))
 pyplot.figure()
-pyplot.imshow(numpy.log10(data[:,512:]))
+pyplot.contourf(numpy.log10(data[:,512:]))
 pyplot.figure()
-pyplot.imshow(numpy.log10(data[:,:512]))
+pyplot.contourf(numpy.log10(data[:,:256]))
 pyplot.show()
