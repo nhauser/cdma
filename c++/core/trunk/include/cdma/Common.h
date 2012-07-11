b0575d78fbf502402b57447417bfa4c84bd6d33a
stephane.poirier <stephane.poirier@synchrotron-soleil.fr>
2012-07-11 14:31:36 +0000

    updated file headers sections

--- c++/core/trunk/include/cdma/Common.h
+++ c++/core/trunk/include/cdma/Common.h
@@ -1,9 +1,9 @@
 //******************************************************************************
 // Copyright (c) 2011 Synchrotron Soleil.
 //
-// This file is part of CDMA library.
+// This file is part of cdma-core library.
 //
-// The CDMA library is free software; you can redistribute it and/or modify it 
+// The cdma-core library is free software; you can redistribute it and/or modify it 
 // under the terms of the GNU General Public License as published by the Free 
 // Software Foundation; either version 2 of the License, or (at your option) 
 // any later version.
