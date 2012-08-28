7e463a33bc022d9c34b21b731d79c6710e03bfad
candelrg@hotmail.com <candelrg@hotmail.com@969d0bd1-f5ba-ef64-2eb3-b0521df2be17>
2012-08-28 14:41:17 +0000

    minor changes

--- java/tools/org.cdma.utilities/branches/soleil/src/main/java/org/cdma/utilities/configuration/ConfigManager.java
+++ java/tools/org.cdma.utilities/branches/soleil/src/main/java/org/cdma/utilities/configuration/ConfigManager.java
@@ -26,34 +26,36 @@ import org.cdma.interfaces.IDataset;
 import org.cdma.utilities.configuration.internal.ConfigGeneric;
 import org.cdma.utilities.configuration.internal.ConfigParameter;
 import org.cdma.utilities.configuration.internal.ConfigParameterStatic;
-import org.cdma.utilities.performance.Benchmarker;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 
 /**
- * ConfigManager is a <b>singleton</b>, only one instance managing all configurations for all plug-ins.
+ * ConfigManager is a <b>singleton</b>, only one instance managing all configurations for all
+ * plug-ins.
  * <p>
- * It associates for each plug-in one or several configurations. 
- * For a given IDataset of the plug-in one and only one configuration should match.
- * It permits to <b>determines some parameters that can be statically or dynamically</b> (according
- * to some conditions or values in dataset) fixed for that specific data model.
+ * It associates for each plug-in one or several configurations. For a given IDataset of the plug-in
+ * one and only one configuration should match. It permits to <b>determines some parameters that can
+ * be statically or dynamically</b> (according to some conditions or values in dataset) fixed for
+ * that specific data model.
  * <p>
  * Each IDataset should match a specific ConfigDataset.
  * 
  * @see ConfigDataset
  * @author rodriguez
- *
+ * 
  */
 
 public class ConfigManager {
-    private static ConfigManager mSingleton;       // Singleton pattern
 
-    private Map<String, IFactory>               mFactories;      // Factories registered by the plug-in name
-    private Map<String, File>                   mFiles;          // Files of configuration for each plug-in
-    private Map<String, Vector<ConfigGeneric> > mConfigurations; // Available configurations for each plug-in
-    private Map<String, Boolean>                mInitialized;    // Does each plug-in been initiated
+    private static ConfigManager mSingleton; // Singleton pattern
+
+    private Map<String, IFactory> mFactories; // Factories registered by the plug-in name
+    private Map<String, File> mFiles; // Files of configuration for each plug-in
+    private Map<String, Vector<ConfigGeneric>> mConfigurations; // Available configurations for each
+                                                                // plug-in
+    private Map<String, Boolean> mInitialized; // Does each plug-in been initiated
 
     /**
      * Get the configuration manager unique instance
@@ -63,10 +65,10 @@ public class ConfigManager {
      * @return an instance of the manager for that plug-in
      */
     public static ConfigManager getInstance(IFactory pluginFactory, String fileName) {
-        synchronized (ConfigManager.class ) {
+        synchronized (ConfigManager.class) {
             // Get the manager singleton instance
-            if( mSingleton == null ) {
-                mSingleton  = new ConfigManager();
+            if (mSingleton == null) {
+                mSingleton = new ConfigManager();
             }
 
             // Initialize the plug-in's available configurations
@@ -77,32 +79,33 @@ public class ConfigManager {
 
     /**
      * Returns the proper configuration for the given dataset
+     * 
      * @param dataset to detect config
      * @return the ConfigDataset that matches the given dataset
      * @throws FileAccessException if unable to load config file
      */
-    public ConfigDataset getConfig( IDataset dataset ) throws NoResultException {
+    public ConfigDataset getConfig(IDataset dataset) throws NoResultException {
         ConfigDataset result = null;
         // Lock the class
-        synchronized (ConfigManager.class ) {
+        synchronized (ConfigManager.class) {
             // Get plug-in name
             String factoryName = dataset.getFactoryName();
 
             // Check that this plug-in has yet loaded its configuration
             Boolean inited = mInitialized.get(factoryName);
-            if( ! inited ) {
+            if (!inited) {
                 try {
                     load(factoryName);
                 }
-                catch(FileAccessException e) {
-                    throw new NoResultException("No result due to a FileAccessException!", e );
+                catch (FileAccessException e) {
+                    throw new NoResultException("No result due to a FileAccessException!", e);
                 }
             }
 
             // Seek for a matching configuration of the plug-in for that dataset
             boolean found = false;
-            for( ConfigGeneric conf: mConfigurations.get(factoryName) ) {
-                if( conf.getCriteria().match(dataset) ) {
+            for (ConfigGeneric conf : mConfigurations.get(factoryName)) {
+                if (conf.getCriteria().match(dataset)) {
                     result = new ConfigDataset(conf, dataset);
                     found = true;
                     break;
@@ -110,29 +113,30 @@ public class ConfigManager {
             }
 
             // If not found throw
-            if( !found ) {
-                throw new NoResultException("NO matching configuration has been found!" + dataset.getLocation());
+            if (!found) {
+                throw new NoResultException("NO matching configuration has been found!"
+                        + dataset.getLocation());
             }
         }
         return result;
     }
 
-
     // ---------------------------------------------------------
-    /// Private methods
+    // / Private methods
     // ---------------------------------------------------------
     /**
-     *  Private constructor
+     * Private constructor
      */
     private ConfigManager() {
-        mFactories      = new HashMap<String, IFactory> ();
-        mFiles          = new HashMap<String, File> ();
-        mConfigurations = new HashMap<String, Vector<ConfigGeneric> > ();
-        mInitialized    = new HashMap<String, Boolean>();
+        mFactories = new HashMap<String, IFactory>();
+        mFiles = new HashMap<String, File>();
+        mConfigurations = new HashMap<String, Vector<ConfigGeneric>>();
+        mInitialized = new HashMap<String, Boolean>();
     }
 
     /**
      * Initialize members
+     * 
      * @param pluginFactory IFactory instance
      * @param fileName configuration file
      **/
@@ -140,18 +144,18 @@ public class ConfigManager {
         // Get the name of the plug-in factory as key entry for map
         String factory = pluginFactory.getName();
 
-        if( ! mFiles.containsKey( factory ) ) {
+        if (!mFiles.containsKey(factory)) {
             // Get the mapping folder that should contains the configuration file
-            String folder = Factory.getPathMappingDictionaryFolder( pluginFactory );
+            String folder = Factory.getPathMappingDictionaryFolder(pluginFactory);
 
-            // Construct the config file 
-            File file = new File( folder + "/" + fileName );
+            // Construct the config file
+            File file = new File(folder + "/" + fileName);
 
             // Construct maps
-            mFiles.put( factory, file );
-            mInitialized.put( factory, false );
-            mConfigurations.put( factory, new Vector<ConfigGeneric>() );
-            mFactories.put( factory, pluginFactory );
+            mFiles.put(factory, file);
+            mInitialized.put(factory, false);
+            mConfigurations.put(factory, new Vector<ConfigGeneric>());
+            mFactories.put(factory, pluginFactory);
         }
     }
 
@@ -166,16 +170,14 @@ public class ConfigManager {
         File file = mFiles.get(name);
 
         // Check configuration file's existence
-        if( file != null ) {
+        if (file != null) {
             // Get the corresponding factory
             IFactory factory = mFactories.get(name);
 
             // Determine the experiment dictionary according to given path
             if (!file.exists()) {
-                throw new FileAccessException("Configuration file for '" + 
-                        factory.getPluginLabel() + "' plug-in doesn't exist:\n" + 
-                        file.getAbsolutePath()
-                        );
+                throw new FileAccessException("Configuration file for '" + factory.getPluginLabel()
+                        + "' plug-in doesn't exist:\n" + file.getAbsolutePath());
             }
 
             // Parse the XML configuration file
@@ -185,10 +187,12 @@ public class ConfigManager {
                 config = xmlFile.build(file);
             }
             catch (JDOMException e1) {
-                throw new FileAccessException("Error while to parsing the configuration!\n" + file.getAbsolutePath() + "\n", e1);
+                throw new FileAccessException("Error while to parsing the configuration!\n"
+                        + file.getAbsolutePath() + "\n", e1);
             }
             catch (IOException e1) {
-                throw new FileAccessException("An I/O error prevent parsing configuration!\n" + file.getAbsolutePath() + "\n", e1);
+                throw new FileAccessException("An I/O error prevent parsing configuration!\n"
+                        + file.getAbsolutePath() + "\n", e1);
             }
 
             // Get the XML file root
@@ -202,7 +206,7 @@ public class ConfigManager {
             Element elem;
             Vector<ConfigGeneric> configurations = new Vector<ConfigGeneric>();
             ConfigGeneric conf;
-            for( Object node : nodes ) {
+            for (Object node : nodes) {
                 elem = (Element) node;
                 conf = new ConfigGeneric(elem, params);
                 configurations.add(conf);
@@ -214,31 +218,32 @@ public class ConfigManager {
 
     /**
      * Parse the global section (i.e: default parameters for each configuration)
+     * 
      * @param root DOM document's root "configuration"
      * @return
      */
-    private List<ConfigParameter> loadGlobalSection( Element root ) {
+    private List<ConfigParameter> loadGlobalSection(Element root) {
         List<ConfigParameter> result = new ArrayList<ConfigParameter>();
         List<?> paramNodes;
         Element elem;
         String name;
         String value;
 
-        // Parser the "global" section 
+        // Parser the "global" section
         List<?> nodes = root.getChildren("global");
-        for( Object node : nodes ) {
+        for (Object node : nodes) {
             // Only consider "java" part
             elem = (Element) node;
             elem = elem.getChild("java");
 
             // Parse all "set"
             paramNodes = elem.getChildren("set");
-            for( Object set : paramNodes ) {
+            for (Object set : paramNodes) {
                 // Construct static parameters
-                elem  = (Element) set;
-                name  = elem.getAttributeValue("name");
+                elem = (Element) set;
+                name = elem.getAttributeValue("name");
                 value = elem.getAttributeValue("value");
-                result.add( new ConfigParameterStatic( name, value ) );
+                result.add(new ConfigParameterStatic(name, value));
             }
         }
 
--- java/tools/org.cdma.utilities/branches/soleil/src/main/java/org/cdma/utilities/configuration/internal/ConfigCriteria.java
+++ java/tools/org.cdma.utilities/branches/soleil/src/main/java/org/cdma/utilities/configuration/internal/ConfigCriteria.java
@@ -15,23 +15,22 @@ import java.util.List;
 
 import org.cdma.interfaces.IDataset;
 import org.cdma.utilities.configuration.internal.ConfigParameter.CriterionValue;
-import org.cdma.utilities.performance.Benchmarker;
 import org.jdom2.Element;
 
 /**
- * This class <b>ConfigCriteria</b> gather a set of criterion. It is used to 
- * determine wether a IDataset is matching to <b>ConfigDataset</b>'s criteria.
+ * This class <b>ConfigCriteria</b> gather a set of criterion. It is used to determine wether a
+ * IDataset is matching to <b>ConfigDataset</b>'s criteria.
  * <p>
- * To be considered as matching the IDataset must be returning <b>CriterionValue.TRUE</b>
- * at the evaluation of each <b>ConfigParameter</b>
+ * To be considered as matching the IDataset must be returning <b>CriterionValue.TRUE</b> at the
+ * evaluation of each <b>ConfigParameter</b>
  * <p>
- * Each <b>ConfigParameter</b> of the contained set of criterion, must return the
- * string form of a <b>CriterionValue</b>. 
- *
- * @see ConfigParameterCriterion ConfigParameterCriterion implements ConfigParameter 
+ * Each <b>ConfigParameter</b> of the contained set of criterion, must return the string form of a
+ * <b>CriterionValue</b>.
+ * 
+ * @see ConfigParameterCriterion ConfigParameterCriterion implements ConfigParameter
  * @see CriterionValue
  * @author rodriguez
- *
+ * 
  */
 public class ConfigCriteria {
     // Private members
@@ -53,10 +52,10 @@ public class ConfigCriteria {
      */
     public void add(Element domCriteria) {
         // Managing criterion
-        if( domCriteria.getName().equals("criteria") ) {
+        if (domCriteria.getName().equals("criteria")) {
             List<?> nodes = domCriteria.getChildren("if");
-            for( Object node : nodes ) {
-                mCriterion.add( new ConfigParameterCriterion((Element) node) );
+            for (Object node : nodes) {
+                mCriterion.add(new ConfigParameterCriterion((Element) node));
             }
         }
     }
@@ -64,38 +63,38 @@ public class ConfigCriteria {
     /**
      * Add the given ConfigParameterCriterion to the set of ConfigParameter.
      * 
-     * @param item ConfigParameterCriterion that will be checked when matching 
+     * @param item ConfigParameterCriterion that will be checked when matching
      */
     public void add(ConfigParameterCriterion item) {
         mCriterion.add(item);
     }
 
     /**
-     * Tells if the given IDataset matches this ConfigCriteria. All clause must be
-     * respected by the given IDataset. 
+     * Tells if the given IDataset matches this ConfigCriteria. All clause must be respected by the
+     * given IDataset.
      * 
      * @param dataset to be checked
      * @return boolean true if each ConfigParameterCriterion are respected in the given IDataset
      */
-    public boolean match( IDataset dataset ) {
+    public boolean match(IDataset dataset) {
         boolean result = true;
-        for( ConfigParameterCriterion criterion : mCriterion ) {
-            if( criterion.getValue(dataset).equals(CriterionValue.FALSE.toString()) ) {
+        for (ConfigParameterCriterion criterion : mCriterion) {
+            if (criterion.getValue(dataset).equals(CriterionValue.FALSE.toString())) {
                 result = false;
                 break;
             }
         }
         return result;
     }
-    
+
     @Override
     public String toString() {
         String result = "Criteria: ";
-        
-        for( ConfigParameterCriterion crit : mCriterion ) {
+
+        for (ConfigParameterCriterion crit : mCriterion) {
             result += "\n" + crit;
         }
-        
+
         return result;
     }
 }
