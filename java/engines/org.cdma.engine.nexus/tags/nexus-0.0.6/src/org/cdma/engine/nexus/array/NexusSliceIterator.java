8e2d72a0cd8c58049f58136d7d1ebf4f0877c8a4
clem.rodrig@gmail.com <clem.rodrig@gmail.com@969d0bd1-f5ba-ef64-2eb3-b0521df2be17>
2012-10-10 14:48:48 +0000

    [SOLEIL][JAVA] Tags
    CDMA core: release 3.2.0
    
    CDMA engines:
     - NeXus: release 0.0.6
     - NetCDF: release 0.0.1
    
    CDMA plug-ins:
     - SOLEIL: release 1.4.8
     - ANSTO: release 1.0.0
    
    CDMA utilities: release 0.0.5

---
+++ java/engines/org.cdma.engine.nexus/tags/nexus-0.0.6/src/org/cdma/engine/nexus/array/NexusSliceIterator.java
@@ -0,0 +1,142 @@
+package org.cdma.engine.nexus.array;
+
+import org.cdma.exception.InvalidRangeException;
+import org.cdma.interfaces.IArray;
+import org.cdma.interfaces.IIndex;
+import org.cdma.interfaces.ISliceIterator;
+
+public final class NexusSliceIterator implements ISliceIterator {
+
+    /// Members
+    private NexusArrayIterator  mIterator;      // iterator of the whole_array
+    private IArray              mArray;         // array of the original shape containing all slices
+    private int[]               mDimension;     // shape of the slice
+    private IArray              mSlice;         // array that will be returned when asking getArrayNext
+    private boolean             mFastMode;      // flag toggling in fast mode: i.e when true the returned IArray will be shared among references
+
+    /// Constructor
+    /**
+     * Create a new NxsSliceIterator that permits to iterate over
+     * slices that are the last dim dimensions of this array.
+     * 
+     * @param array source of the slice
+     * @param dim returned dimensions
+     */
+    public NexusSliceIterator(final IArray array, final int dim) throws InvalidRangeException {
+        // If ranks are equal, make sure at least one iteration is performed.
+        // We cannot use 'reshape' (which would be ideal) as that creates a
+        // new copy of the array storage and so is unusable
+        mArray = array;
+        int[] shape = mArray.getShape();
+        int[] rangeList = shape.clone();
+        int[] origin = mArray.getIndex().getOrigin().clone();
+        long[] stride = mArray.getIndex().getStride().clone();
+        int rank = mArray.getRank();
+
+        // shape to make a 2D array from multiple-dim array
+        mDimension = new int[dim];
+        System.arraycopy(shape, shape.length - dim, mDimension, 0, dim);
+        for (int i = 0; i < dim; i++) {
+            rangeList[rank - i - 1] = 1;
+        }
+        // Create an iterator over the higher dimensions. We leave in the
+        // final dimensions so that we can use the getCurrentCounter method
+        // to create an origin.
+
+        IIndex index = mArray.getIndex();
+
+        // As we get a reference on array's IIndex we directly modify it
+        index.setOrigin(origin);
+        index.setStride(stride);
+        index.setShape(rangeList);
+
+        mIterator = new NexusArrayIterator(mArray, index, false);
+        mFastMode = false;
+
+    }
+
+    /// Public methods
+    @Override
+    public IArray getArrayNext() throws InvalidRangeException {
+        next();
+        if( mSlice == null ) {
+            createSlice();
+        }
+        else {
+            updateSlice();
+        }
+        // If fast iteration mode is ON then all returned arrays will be shared
+        if( ! mFastMode ) {
+            mSlice = mSlice.copy(false);
+        }
+        return mSlice;
+    }
+
+    @Override
+    public int[] getSliceShape() throws InvalidRangeException {
+        return mDimension.clone();
+    }
+
+    public int[] getSlicePosition() {
+        return mIterator.getCounter();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return mIterator.hasNext();
+    }
+
+    @Override
+    public void next() {
+        mIterator.incrementIndex();
+    }
+
+    @Override
+    public String getFactoryName() {
+        return mArray.getFactoryName();
+    }
+    
+    public boolean getFastMode() {
+        return mFastMode;
+    }
+    
+    public void setFastMode( boolean activate ) {
+        mFastMode = activate;
+    }
+
+    // ---------------------------------------------------------
+    /// Private methods
+    // ---------------------------------------------------------
+    private void createSlice() throws InvalidRangeException {
+        mSlice = mArray.copy(false);
+        updateSlice();
+    }
+    
+    private void updateSlice() throws InvalidRangeException {
+        int i = 0;
+        int[] iShape = mArray.getShape();
+        int[] iOrigin = mArray.getIndex().getOrigin();
+        int[] iCurPos = mIterator.getCounter();
+        for (int pos : iCurPos) {
+            if (pos >= iShape[i]) {
+                mSlice = null;
+                return;
+            }
+            iOrigin[i] += pos;
+            i++;
+        }
+        
+        java.util.Arrays.fill(iShape, 1);
+        System.arraycopy(mDimension, 0, iShape, iShape.length - mDimension.length, mDimension.length);
+        IIndex index;
+        try {
+            index = mArray.getIndex().clone();
+            index.setShape(iShape);
+            index.setOrigin(iOrigin);
+            index.reduce();
+            mSlice.setIndex(index);
+        } catch (CloneNotSupportedException e) {
+            mSlice = null;
+        }
+    }
+}
---
+++ java/plugins/org.cdma.plugin.soleil/tags/NexusSoleil-1.4.8/src/org/cdma/plugin/soleil/NxsDatasource.java
@@ -0,0 +1,239 @@
+package org.cdma.plugin.soleil;
+
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Level;
+
+import org.cdma.Factory;
+import org.cdma.exception.NoResultException;
+import org.cdma.interfaces.IDatasource;
+import org.cdma.interfaces.IGroup;
+import org.cdma.plugin.soleil.internal.DetectedSource;
+import org.cdma.plugin.soleil.navigation.NxsDataset;
+
+import fr.soleil.nexus.NexusNode;
+import fr.soleil.nexus.PathNexus;
+
+public final class NxsDatasource implements IDatasource {
+    private static final int MAX_SOURCE_BUFFER_SIZE = 200;
+    private static HashMap<String, DetectedSource> mDetectedSources; // map of analyzed URIs
+    private static NxsDatasource datasource;
+
+    public static NxsDatasource getInstance() {
+        synchronized (NxsDatasource.class ) {
+            if( datasource == null ) {
+                datasource  = new NxsDatasource();
+            }
+        }
+        return datasource;
+    }
+    
+    @Override
+    public String getFactoryName() {
+        return NxsFactory.NAME;
+    }
+
+    @Override
+    public boolean isReadable(URI target) {
+        boolean result = false;
+        DetectedSource source = getSource(target);
+        if (source != null) {
+            result = source.isReadable();
+        }
+        return result;
+    }
+
+    @Override
+    public boolean isProducer(URI target) {
+        boolean result = false;
+        DetectedSource source = getSource(target);
+        if (source != null) {
+            result = source.isProducer();
+        }
+        return result;
+    }
+
+    @Override
+    public boolean isExperiment(URI target) {
+        boolean result = false;
+        DetectedSource source = getSource(target);
+        if (source != null) {
+            result = source.isExperiment();
+        }
+        return result;
+    }
+
+    @Override
+    public boolean isBrowsable(URI target) {
+        boolean result = false;
+        DetectedSource source = getSource(target);
+        if (source != null) {
+            result = source.isBrowsable();
+        }
+        return result;
+    }
+
+    @Override
+    public List<URI> getValidURI(URI target) {
+        List<URI> result = new ArrayList<URI>();
+
+        DetectedSource source = getSource(target);
+        if (source != null) {
+            if (source.isFolder() && !source.isDatasetFolder()) {
+                File folder = new File(target.getPath());
+                for (File file : folder.listFiles()) {
+                    result.add(file.toURI());
+                }
+            }
+            else {
+                if (source.isReadable() && source.isBrowsable()) {
+                    try {
+                        String uri = target.toString();
+                        String sep = target.getFragment() == null ? "#" : "";
+
+                        NxsDataset dataset = NxsDataset.instanciate(target);
+                        IGroup group = dataset.getRootGroup();
+                        for (IGroup node : group.getGroupList()) {
+                            result.add(URI.create(uri + sep
+                                    + URLEncoder.encode("/" + node.getShortName(), "UTF-8")));
+                        }
+
+                    }
+                    catch (NoResultException e) {
+                        Factory.getLogger().log( Level.WARNING, e.getMessage());
+                    }
+                    catch (UnsupportedEncodingException e) {
+                        Factory.getLogger().log( Level.WARNING, e.getMessage());
+                    }
+                }
+            }
+        }
+        return result;
+    }
+    
+    @Override
+    public URI getParentURI( URI target ) {
+        URI result = null;
+        
+        if ( isBrowsable(target) || isReadable(target) ) {
+
+            File current = new File(target.getPath());
+            String fragment = target.getFragment();
+            String filePath = "";
+            
+            if( fragment == null ) {
+                current = current.getParentFile();
+                filePath = current.toURI().toString();
+                fragment = "";
+            }
+            else {
+                filePath = current.toURI().toString();
+                
+                try {
+                    fragment = URLDecoder.decode(fragment, "UTF-8");
+                    NexusNode[] nodes = PathNexus.splitStringToNode(fragment);
+                    fragment = "";
+                    for ( int i = 0; i < nodes.length - 1; i++ ) {
+                        fragment += "/" + nodes[i].getNodeName();
+                    }
+
+                    if( ! fragment.isEmpty() ) {
+                        fragment = "#" + URLEncoder.encode(fragment, "UTF-8");
+                    }
+                } catch (UnsupportedEncodingException e) {
+                    Factory.getLogger().log( Level.WARNING, e.getMessage());
+                }
+                
+            }
+            
+            result = URI.create(filePath + fragment);
+        }
+        
+        return result;
+    }
+
+    @Override
+    public String[] getURIParts(URI target) {
+        List<String> parts = new ArrayList<String>();
+//        if ( isBrowsable(target) || isReadable(target) ) {
+            String path = target.getPath();
+            String fragment = target.getFragment();
+            for( String part : path.split( "/" ) ) {
+                if( part != null && ! part.isEmpty() ) {
+                    parts.add(part);
+                }
+            }
+
+            if (fragment != null) {
+                try {
+                    fragment = URLDecoder.decode(fragment, "UTF-8");
+                    NexusNode[] nodes = PathNexus.splitStringToNode(fragment);
+                    for (NexusNode node : nodes ) {
+                        parts.add( node.getNodeName() );
+                    }
+                }
+                catch (UnsupportedEncodingException e) {
+                }
+            }
+//        }
+
+        return parts.toArray(new String[] {});
+    }
+
+    @Override
+    public long getLastModificationDate(URI target) {
+        long last = 0;
+        if (isReadable(target) || isBrowsable(target)) {
+            File file = new File(target.getPath());
+            if (file.exists()) {
+                last = file.lastModified();
+            }
+        }
+        return last;
+    }
+
+    // ---------------------------------------------------------
+    // / private methods
+    // ---------------------------------------------------------
+    private DetectedSource getSource(URI uri) {
+        DetectedSource source = null;
+        synchronized (mDetectedSources) {
+            source = mDetectedSources.get(uri.toString());
+            if (source == null) {
+                if (mDetectedSources.size() > MAX_SOURCE_BUFFER_SIZE) {
+                    int i = MAX_SOURCE_BUFFER_SIZE / 2;
+                    List<String> remove = new ArrayList<String>();
+                    for (String key : mDetectedSources.keySet()) {
+                        remove.add(key);
+                        if (i-- < 0) {
+                            break;
+                        }
+                    }
+                    for (String key : remove) {
+                        mDetectedSources.remove(key);
+                    }
+                }
+
+                source = new DetectedSource(uri);
+                mDetectedSources.put(uri.toString(), source);
+            }
+        }
+        return source;
+    }
+    
+    private NxsDatasource() {
+        if (mDetectedSources == null) {
+            synchronized (NxsDatasource.class) {
+                if (mDetectedSources == null) {
+                    mDetectedSources = new HashMap<String, DetectedSource>();
+                }
+            }
+        }
+    }
+}
