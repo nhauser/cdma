cf0bd8f869dbc894582d7597de10fea591a2e9e0
clem.rodrig@gmail.com <clem.rodrig@gmail.com@969d0bd1-f5ba-ef64-2eb3-b0521df2be17>
2012-12-04 10:43:11 +0000

    [SOLEIL][Java] Trunk
    CDMA plug-in SOLEIL Nexus:
      - use of default IIndex and IRange implementations from utilities

--- java/engines/org.cdma.engine.nexus/trunk/src/org/cdma/engine/nexus/array/NexusArrayIterator.java
+++ java/engines/org.cdma.engine.nexus/trunk/src/org/cdma/engine/nexus/array/NexusArrayIterator.java
@@ -12,195 +12,178 @@ package org.cdma.engine.nexus.array;
 import org.cdma.interfaces.IArray;
 import org.cdma.interfaces.IArrayIterator;
 import org.cdma.interfaces.IIndex;
+import org.cdma.utilities.memory.ArrayTools;
 
 public final class NexusArrayIterator implements IArrayIterator {
-    /// Members
-    private IArray  mArray;
-    private IIndex  mIndex;
-    private Object  mCurrent;
-    private boolean mAccess; // Indicates that this can access the storage memory or not
+	// / Members
+	private IArray mArray;
+	private IIndex mIndex;
+	private Object mCurrent;
+	private boolean mAccess; // Indicates that this can access the storage memory or not
 
-    public NexusArrayIterator(NexusArray array)
-    {
-	mArray  = array;
-	// [ANSTO][Tony][2011-08-31] Should mAccess set to true for NxsArrayInterface??
-	// If m_access is set to false, next() does not work.
-	// [SOLEIL][Clement][2011-11-22] Yes it should. It indicates that the iterator shouldn't access memory. In case of hudge matrix the next() will update m_current (i.e. value), but the underlying NeXus engine will automatically load the part corresponding to the view defined by this iterator, which can lead to java heap space memory exception (see NxsArray : private Object getData() ) 
-	mAccess = true;
-	try {
-	    mIndex = array.getIndex().clone();
-	    mIndex.set( new int[mIndex.getRank()] );
-	    mIndex.setDim( mIndex.getRank() - 1, -1);
-	} catch (CloneNotSupportedException e) {
+	public NexusArrayIterator(NexusArray array) {
+		mArray = array;
+		// [ANSTO][Tony][2011-08-31] Should mAccess set to true for NxsArrayInterface?? If m_access is set to false, next() does not work.
+		// [SOLEIL][Clement][2011-11-22] Yes it should. It indicates that the iterator shouldn't access memory. 
+		// In case of hudge matrix the next() will update m_current (i.e. value), but the underlying NeXus engine
+		// will automatically load the part corresponding to the view defined by this iterator, 
+		// which can lead to java heap space memory exception (see NxsArray : private Object getData() )
+		mAccess = true;
+		try {
+			mIndex = array.getIndex().clone();
+			mIndex.set(new int[mIndex.getRank()]);
+			mIndex.setDim(mIndex.getRank() - 1, -1);
+		} catch (CloneNotSupportedException e) {
+		}
 	}
-    }
 
-    public NexusArrayIterator(IArray array, IIndex index) {
-	this(array, index, true);
-    }
-
-    public NexusArrayIterator(IArray array, IIndex index, boolean accessData) {
-	int[] count = index.getCurrentCounter();
-	mArray     = array;
-	mIndex     = index;
-	mAccess    = accessData;
-	count[mIndex.getRank() - 1]--;
-	mIndex.set( count );
-    }
-
-    @Override
-    public boolean getBooleanNext() {
-	incrementIndex(mIndex);
-	return mArray.getBoolean(mIndex);
-    }
-
-    @Override
-    public byte getByteNext() {
-	incrementIndex(mIndex);
-	return mArray.getByte(mIndex);
-    }
-
-    @Override
-    public char getCharNext() {
-	return ((Character) next()).charValue();
-    }
-
-    @Override
-    public int[] getCounter() {
-	return mIndex.getCurrentCounter();
-    }
-
-    @Override
-    public double getDoubleNext() {
-	incrementIndex(mIndex);
-	return mArray.getDouble(mIndex);
-    }
-
-    @Override
-    public float getFloatNext()  {
-	incrementIndex(mIndex);
-	return mArray.getFloat(mIndex);
-    }
-
-    @Override
-    public int getIntNext() {
-	incrementIndex(mIndex);
-	return mArray.getInt(mIndex);
-    }
-
-    @Override
-    public long getLongNext() {
-	incrementIndex(mIndex);
-	return mArray.getLong(mIndex);
-    }
-
-    @Override
-    public Object getObjectNext() {
-	return next();
-    }
-
-    @Override
-    public short getShortNext() {
-	incrementIndex(mIndex);
-	return mArray.getShort(mIndex);
-    }
-
-    @Override
-    public boolean hasNext()
-    {
-	long index = mIndex.currentElement();
-	long last  = mIndex.lastElement();
-	return ( index < last && index >= -1);
-    }
-
-    @Override
-    public Object next()
-    {
-	incrementIndex(mIndex);
-	if( mAccess ) {
-	    long currentPos = mIndex.currentElement();
-	    if( currentPos <= mIndex.lastElement() && currentPos != -1 ) {
-		mCurrent = mArray.getObject(mIndex);
-	    }
-	    else {
-		mCurrent = null;
-	    }
+	public NexusArrayIterator(IArray array, IIndex index) {
+		this(array, index, true);
 	}
-	return mCurrent;
-    }
 
-    @Override
-    public void setBoolean(boolean val) {
-	setObject(val);
-    }
-
-    @Override
-    public void setByte(byte val) {
-	setObject(val);
-    }
-
-    @Override
-    public void setChar(char val) {
-	setObject(val);
-    }
-
-    @Override
-    public void setDouble(double val) {
-	setObject(val);
-    }
-
-    @Override
-    public void setFloat(float val) {
-	setObject(val);
-    }
-
-    @Override
-    public void setInt(int val) {
-	setObject(val);
-    }
-
-    @Override
-    public void setLong(long val) {
-	setObject(val);
-    }
-
-    @Override
-    public void setObject(Object val) {
-	mCurrent = val;
-	mArray.setObject(mIndex, val);
-    }
-
-    @Override
-    public void setShort(short val) {
-	setObject(val);
-    }
-
-    public static void incrementIndex(IIndex index)
-    {
-	int[] current = index.getCurrentCounter();
-	int[] shape = index.getShape();
-	for( int i = current.length - 1; i >= 0; i-- )
-	{
-	    if( current[i] + 1 >= shape[i] && i > 0)
-	    {
-		current[i] = 0;
-	    }
-	    else
-	    {
-		current[i]++;
-		index.set(current);
-		return;
-	    }
+	public NexusArrayIterator(IArray array, IIndex index, boolean accessData) {
+		int[] count = index.getCurrentCounter();
+		mArray = array;
+		mIndex = index;
+		mAccess = accessData;
+		count[mIndex.getRank() - 1]--;
+		mIndex.set(count);
 	}
-    }
 
-    @Override
-    public String getFactoryName() {
-	return mArray.getFactoryName();
-    }
+	@Override
+	public boolean getBooleanNext() {
+		incrementIndex();
+		return mArray.getBoolean(mIndex);
+	}
 
-    /// protected method
-    protected void incrementIndex() {
-	NexusArrayIterator.incrementIndex(mIndex);
-    }
+	@Override
+	public byte getByteNext() {
+		incrementIndex();
+		return mArray.getByte(mIndex);
+	}
+
+	@Override
+	public char getCharNext() {
+		return ((Character) next()).charValue();
+	}
+
+	@Override
+	public int[] getCounter() {
+		return mIndex.getCurrentCounter();
+	}
+
+	@Override
+	public double getDoubleNext() {
+		incrementIndex();
+		return mArray.getDouble(mIndex);
+	}
+
+	@Override
+	public float getFloatNext() {
+		incrementIndex();
+		return mArray.getFloat(mIndex);
+	}
+
+	@Override
+	public int getIntNext() {
+		incrementIndex();
+		return mArray.getInt(mIndex);
+	}
+
+	@Override
+	public long getLongNext() {
+		incrementIndex();
+		return mArray.getLong(mIndex);
+	}
+
+	@Override
+	public Object getObjectNext() {
+		return next();
+	}
+
+	@Override
+	public short getShortNext() {
+		incrementIndex();
+		return mArray.getShort(mIndex);
+	}
+
+	@Override
+	public boolean hasNext() {
+		long index = mIndex.currentElement();
+		long last = mIndex.lastElement();
+		return (index < last && index >= -1);
+	}
+
+	@Override
+	public Object next() {
+		incrementIndex();
+		if (mAccess) {
+			long currentPos = mIndex.currentElement();
+			if (currentPos <= mIndex.lastElement() && currentPos != -1) {
+				mCurrent = mArray.getObject(mIndex);
+			} else {
+				mCurrent = null;
+			}
+		}
+		return mCurrent;
+	}
+
+	@Override
+	public void setBoolean(boolean val) {
+		setObject(val);
+	}
+
+	@Override
+	public void setByte(byte val) {
+		setObject(val);
+	}
+
+	@Override
+	public void setChar(char val) {
+		setObject(val);
+	}
+
+	@Override
+	public void setDouble(double val) {
+		setObject(val);
+	}
+
+	@Override
+	public void setFloat(float val) {
+		setObject(val);
+	}
+
+	@Override
+	public void setInt(int val) {
+		setObject(val);
+	}
+
+	@Override
+	public void setLong(long val) {
+		setObject(val);
+	}
+
+	@Override
+	public void setObject(Object val) {
+		mCurrent = val;
+		mArray.setObject(mIndex, val);
+	}
+
+	@Override
+	public void setShort(short val) {
+		setObject(val);
+	}
+
+	@Override
+	public String getFactoryName() {
+		return mArray.getFactoryName();
+	}
+
+	// / protected method
+	protected void incrementIndex() {
+		int[] current = mIndex.getCurrentCounter();
+		int[] shape = mIndex.getShape();
+		ArrayTools.incrementCounter(current, shape);
+		mIndex.set(current);
+	}
 }
