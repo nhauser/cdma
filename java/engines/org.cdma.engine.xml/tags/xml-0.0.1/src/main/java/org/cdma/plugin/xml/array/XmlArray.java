4d6f554623007d47015604c0c5d1257b0279dd1f
qa-soleil@synchrotron-soleil.fr <qa-soleil@synchrotron-soleil.fr@969d0bd1-f5ba-ef64-2eb3-b0521df2be17>
2013-01-25 09:55:48 +0000

    Remote copy.

---
+++ java/engines/org.cdma.engine.xml/tags/xml-0.0.1/src/main/java/org/cdma/plugin/xml/array/XmlArray.java
@@ -0,0 +1,120 @@
+package org.cdma.plugin.xml.array;
+
+import org.cdma.arrays.DefaultArrayInline;
+import org.cdma.exception.InvalidArrayTypeException;
+import org.cdma.interfaces.IIndex;
+
+public class XmlArray extends DefaultArrayInline {
+
+	public XmlArray(String factory, String value) throws InvalidArrayTypeException {
+		super( factory, new String[] {value}, new int[1] );
+	}
+
+	@Override
+	public boolean getBoolean(IIndex ima) {
+		boolean result = false;
+		try {
+			result = Boolean.parseBoolean( (String) getData() );
+		} catch (NumberFormatException e) {
+			// nothing to do
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	@Override
+	public byte getByte(IIndex ima) {
+		byte result = 0;
+		try {
+			result = Byte.parseByte( (String) getData() );
+		} catch (NumberFormatException e) {
+			// nothing to do
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	@Override
+	public char getChar(IIndex ima) {
+		char result = 0;
+		String value = (String) getData();
+		try {
+			if (value.length() == 1) {
+				result = value.charAt(0);
+			}
+		} catch (IndexOutOfBoundsException e) {
+			// nothing to be done
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	@Override
+	public double getDouble(IIndex ima) {
+		double result = 0.0;
+		try {
+			result = Double.parseDouble( (String) getData() );
+		} catch (NumberFormatException e) {
+			// nothing to do
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	@Override
+	public float getFloat(IIndex ima) {
+		float result = 0;
+		try {
+			result = Float.parseFloat( (String) getData() );
+		} catch (NumberFormatException e) {
+			// nothing to do
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	@Override
+	public int getInt(IIndex ima) {
+		int result = 0;
+		try {
+			result = Integer.parseInt( (String) getData() );
+		} catch (NumberFormatException e) {
+			// nothing to do
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	@Override
+	public long getLong(IIndex ima) {
+		long result = 0;
+		try {
+			result = Long.parseLong( (String) getData() );
+		} catch (NumberFormatException e) {
+			// nothing to do
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	@Override
+	public short getShort(IIndex ima) {
+		short result = 0;
+		try {
+			result = Short.parseShort( (String) getData() );
+		} catch (NumberFormatException e) {
+			// nothing to do
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+}
