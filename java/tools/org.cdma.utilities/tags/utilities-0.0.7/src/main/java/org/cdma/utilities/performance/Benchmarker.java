3abe6542cc5eae0c1b6df0efe1044646a3ea2b06
qa-soleil@synchrotron-soleil.fr <qa-soleil@synchrotron-soleil.fr@969d0bd1-f5ba-ef64-2eb3-b0521df2be17>
2013-02-21 09:27:38 +0000

    Remote copy.

---
+++ java/tools/org.cdma.utilities/tags/utilities-0.0.7/src/main/java/org/cdma/utilities/performance/Benchmarker.java
@@ -0,0 +1,145 @@
+//******************************************************************************
+// Copyright (c) 2011 Synchrotron Soleil.
+// The CDMA library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License, or (at your option)
+// any later version.
+// Contributors :
+//    Clément Rodriguez (clement.rodriguez@synchrotron-soleil.fr)
+// See AUTHORS file
+//******************************************************************************
+package org.cdma.utilities.performance;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+import java.util.logging.Level;
+
+import org.cdma.Factory;
+
+public class Benchmarker {
+    private static Map<String, Long> timers   = new TreeMap<String, Long>();
+    private static Map<String, Long> counters = new TreeMap<String, Long>();
+    private static Map<String, Long> starters = new TreeMap<String, Long>();
+    private static Map<String, Long> nbcalls  = new TreeMap<String, Long>();
+    private static Map<String, Long> nbthread = new TreeMap<String, Long>();
+
+    public static void start(String label) {
+        Long currentTime = System.currentTimeMillis();
+        synchronized (Benchmarker.class) {
+            if (counters.containsKey(label)) {
+                Long counter = counters.get(label);
+                Long call = nbcalls.get(label);
+                if (counter <= 0) {
+                    counters.put(label, (long) 1);
+                    starters.put(label, currentTime);
+                } else {
+                    counters.put(label, ++counter);
+                    Long thread = nbthread.get(label);
+                    if( thread < counter ) {
+                        nbthread.put(label, counter);
+                    }
+                }
+                nbcalls.put(label, ++call);
+            } else {
+                counters.put(label, (long) 1);
+                starters.put(label, currentTime);
+                timers.put(label, (long) 0);
+                nbcalls.put(label, (long) 1);
+                nbthread.put(label, (long) 1);
+            }
+        }
+    }
+
+    public static void stop(String label) {
+        Long currentTime = System.currentTimeMillis();
+        synchronized (Benchmarker.class) {
+            if (counters.containsKey(label)) {
+                Long counter = counters.get(label);
+                if (counter > 1) {
+                    counter--;
+                    counters.put(label, counter);
+                } else if (counter > 0) {
+                    counter--;
+                    Long starter = starters.get(label);
+                    Long time = timers.get(label);
+                    time += (currentTime - starter);
+                    timers.put(label, time);
+                    starters.put(label, (long) 0);
+                    counters.put(label, counter);
+                } else {
+                    Factory.getLogger().log( Level.INFO, ">>>>>>>>>>>>>>>>>> Benchmark  <<<<<<<<<<<<<<<<<<<<<<");
+                    Factory.getLogger().log( Level.INFO, "To much stop for: " + label);
+                    Factory.getLogger().log( Level.INFO, ">>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+                }
+            } else {
+                Factory.getLogger().log( Level.INFO, ">>>>>>>>>>>>>>>>>> Benchmark  <<<<<<<<<<<<<<<<<<<<<<");
+                Factory.getLogger().log( Level.INFO, "Stopping inexistent timer: " + label);
+                Factory.getLogger().log( Level.INFO, ">>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+            }
+        }
+    }
+
+    public static Map<String, Long> getTimers() {
+        return timers;
+    }
+
+    public static String print() {
+        StringBuilder result = new StringBuilder();
+        for (String label : timers.keySet()) {
+            result.append( print( label
+                    + ": "
+                    + ((Benchmarker.getTimers().get(label)) / (float) 1000) + " s",
+                    60) );
+            result.append( print( " nb calls: " + nbcalls.get(label), 20 ) );
+            result.append( print( " max thread: " +  nbthread.get(label), 20) );
+            result.append( ( " canonical cost: " + ( Benchmarker.getTimers().get(label) / (float) nbcalls.get(label) ) + " ms\n") );
+        }
+        return result.toString();
+    }
+
+    private static String print(String value, int length ) {
+        String result = value;
+
+        if( value.length() < length ) {
+            for( int i = 0; i < length - value.length(); i++ ) {
+                result += " ";
+            }
+        }
+        return result;
+    }
+
+    public static void reset() {
+        synchronized (Benchmarker.class) {
+            boolean reset = true;
+            List<String> running = new ArrayList<String>();
+            for( Entry<String,Long> counter : counters.entrySet() ) {
+                if( counter.getValue() > 0 ) {
+                	running.add(counter.getKey());
+                    reset = false;
+                }
+            }
+            if( reset ) {
+                timers   = new TreeMap<String, Long>();
+                counters = new TreeMap<String, Long>();
+                starters = new TreeMap<String, Long>();
+                nbcalls  = new TreeMap<String, Long>();
+                nbthread = new TreeMap<String, Long>();
+            }
+            else {
+            	StringBuffer log = new StringBuffer();
+            	log.append( ">>>>>>>>>>>>>>>>>> Benchmark  <<<<<<<<<<<<<<<<<<<<<<\n" );
+            	log.append( "Timers are still running!!!!\n" );
+            	log.append( "Please stop the following timers:\n" );
+                for( String timer : running ) {
+                	log.append( "   - " + timer + "\n" );
+                }
+                log.append( ">>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<" );
+                Factory.getLogger().log( Level.INFO, log.toString() );
+            }
+        }
+    }
+
+}
