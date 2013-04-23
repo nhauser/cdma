d4e1927006ebca861b94ea9dda529a33d46e5111
gregory.viguier@gmail.com <gregory.viguier@gmail.com@969d0bd1-f5ba-ef64-2eb3-b0521df2be17>
2013-04-23 13:12:13 +0000

    Initial Import

---
+++ java/plugins/org.cdma.plugin.soleil/branches/hdf/src/org/cdma/plugin/soleil/external/CreateVirtualItem.java
@@ -0,0 +1,76 @@
+//******************************************************************************
+// Copyright (c) 2011 Synchrotron Soleil.
+// The CDMA library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License, or (at your option)
+// any later version.
+// Contributors :
+// See AUTHORS file
+//******************************************************************************
+package org.cdma.plugin.soleil.external;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.cdma.dictionary.Context;
+import org.cdma.dictionary.IPluginMethod;
+import org.cdma.exception.CDMAException;
+import org.cdma.interfaces.IAttribute;
+import org.cdma.interfaces.IContainer;
+import org.cdma.interfaces.IGroup;
+import org.cdma.plugin.soleil.NxsFactory;
+import org.cdma.plugin.soleil.array.NxsArray;
+import org.cdma.plugin.soleil.navigation.NxsDataItem;
+import org.cdma.plugin.soleil.utils.NxsPath;
+import org.cdma.utils.Utilities.ModelType;
+
+/**
+ * Create a list of IDataItem that are empty from a IGroup list. Created items have no data linked.
+ * 
+ * @param context
+ * @throws CDMAException
+ */
+
+public class CreateVirtualItem implements IPluginMethod {
+
+    @Override
+    public String getFactoryName() {
+        return NxsFactory.NAME;
+    }
+
+    @Override
+    public void execute(Context context) throws CDMAException {
+        List<IContainer> inList = context.getContainers();
+        List<IContainer> outList = new ArrayList<IContainer>();
+
+        NxsDataItem item;
+        NxsPath path;
+        NxsArray array;
+        String name;
+        for (IContainer container : inList) {
+            if (container.getModelType().equals(ModelType.Group)) {
+                name = container.getName();
+                path = new NxsPath(NxsPath.splitStringToNode(container.getLocation()));
+                item = new NxsDataItem();
+                array = new NxsArray(name.toCharArray(), new int[] {name.length()});
+                item.setName(name);
+                item.setShortName(container.getShortName());
+                item.setDataset(container.getDataset());
+                item.setPath(path);
+                item.setParent( (IGroup) container);
+                item.setCachedData(array, false);
+                for (IAttribute attr : container.getAttributeList()) {
+                    item.addOneAttribute(attr);
+                }
+                outList.add(item);
+            }
+            else {
+                outList.add(container);
+            }
+        }
+
+        // Update context
+        context.setContainers(outList);
+    }
+
+}
---
+++ java/plugins/org.cdma.plugin.soleil/branches/hdf/src/org/cdma/plugin/soleil/internal/NexusDatasetImpl.java
@@ -0,0 +1,42 @@
+//******************************************************************************
+// Copyright (c) 2011 Synchrotron Soleil.
+// The CDMA library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License, or (at your option)
+// any later version.
+// Contributors :
+// See AUTHORS file
+//******************************************************************************
+package org.cdma.plugin.soleil.internal;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+
+import navigation.HdfDataset;
+
+import org.cdma.Factory;
+import org.cdma.dictionary.LogicalGroup;
+import org.cdma.exception.FileAccessException;
+import org.cdma.plugin.soleil.NxsFactory;
+
+public class NexusDatasetImpl extends HdfDataset {
+
+    // ---------------------------------------------------------
+    // Internal class that concretes the abstract NexusDataset
+    // ---------------------------------------------------------
+    public NexusDatasetImpl(File nexusFile) throws FileAccessException {
+        super(NxsFactory.NAME, nexusFile);
+        try {
+            open();
+        }
+        catch (IOException e) {
+            Factory.getLogger().log(Level.SEVERE, e.getMessage());
+        }
+    }
+
+    @Override
+    public LogicalGroup getLogicalRoot() {
+        return new LogicalGroup(null, null, this, false);
+    }
+}
