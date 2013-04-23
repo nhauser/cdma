167e370a4619c5a34a1f6a5b9454606a3af9aeb5
qa-soleil@synchrotron-soleil.fr <qa-soleil@synchrotron-soleil.fr@969d0bd1-f5ba-ef64-2eb3-b0521df2be17>
2013-04-23 09:40:42 +0000

    Remote copy.

---
+++ java/plugins/org.cdma.plugin.soleil/tags/NexusSoleil-1.4.13/src/org/cdma/plugin/soleil/utils/NxsConstant.java
@@ -0,0 +1,24 @@
+//******************************************************************************
+// Copyright (c) 2011 Synchrotron Soleil.
+// The CDMA library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License, or (at your option)
+// any later version.
+// Contributors :
+// See AUTHORS file
+//******************************************************************************
+package org.cdma.plugin.soleil.utils;
+
+public class NxsConstant {
+    // Parameters in configuration
+    public static final String BEAMLINE    = "BEAMLINE";
+    public static final String MODEL       = "MODEL";
+    public static final String FAST_MODE   = "fast_iterator";
+    public static final String LOG_DEBUG   = "log_debug";
+    public static final String BUFFER_SIZE = "N4T_buffer_size";
+    
+    // Attribute 
+    public static final String ATTR_EQUIPMENT = "equipment";
+    public static final String ATTR_REGION    = "region";
+    public static final String ATTR_SCAN      = "acquisition_sequence";
+}
