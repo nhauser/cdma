66f66c89546bbedfdbc411750b958f187cfcb53c
gregory.viguier@gmail.com <gregory.viguier@gmail.com@969d0bd1-f5ba-ef64-2eb3-b0521df2be17>
2013-09-04 12:44:44 +0000

    Add tests

---
+++ java/engines/org.cdma.engine.hdf/trunk/src/test/java/org/cdma/engine/hdf/ReadTests.java
@@ -0,0 +1,78 @@
+package org.cdma.engine.hdf;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.cdma.engine.hdf.navigation.HdfDataset;
+import org.cdma.exception.NoResultException;
+import org.cdma.interfaces.IArray;
+import org.cdma.interfaces.IContainer;
+import org.cdma.interfaces.IDataItem;
+import org.cdma.interfaces.IDataset;
+import org.cdma.interfaces.IGroup;
+import org.junit.Test;
+
+public class ReadTests {
+
+    @Test
+    public void aReadFile() throws IOException, NoResultException {
+        IDataset dataSet = new HdfDataset("HDF", WriteTests.FIRST_FILE_TO_WRITE);
+        IGroup root = dataSet.getRootGroup();
+        if (root != null) {
+
+            // Test ROOT
+            List<IGroup> groupList = root.getGroupList();
+            assertEquals(2, groupList.size());
+            assertNull(root.getParentGroup());
+
+            // Test IGroup
+            IGroup group1 = root.getGroup("group1");
+            assertNotNull(group1);
+            assertEquals(2, group1.getAttributeList().size());
+            assertEquals("Attribute List size", 2, group1.getAttributeList().size());
+            assertEquals("group1", group1.getShortName());
+            assertEquals("/group1", group1.getName());
+            assertEquals(root, group1.getRootGroup());
+            assertEquals(dataSet, group1.getDataset());
+            assertTrue(group1.isEntry());
+            assertFalse(group1.isRoot());
+
+            // Test IDataItem
+            IDataItem data1 = group1.getDataItem("data1");
+            assertNotNull(data1);
+            IArray iArray = data1.getData();
+            Object storage = iArray.getStorage();
+            int[] array = (int[]) storage;
+            int[] expected = { 0, 1, 2, 3 };
+            assertArrayEquals(expected, array);
+            assertEquals(int.class, iArray.getElementType());
+            assertEquals(root, data1.getRootGroup());
+            assertEquals(group1, data1.getParentGroup());
+            assertEquals(dataSet, data1.getDataset());
+            assertEquals("/group1/data1", data1.getName());
+            assertEquals("data1", data1.getShortName());
+
+            // Test Navigation
+            IContainer container = root.findContainerByPath("/group1/data1");
+            assertNotNull(container);
+
+            List<IContainer> containers = root.findAllContainerByPath("/group1/data1");
+            assertNotNull(containers);
+            assertTrue(containers.size() == 1);
+
+            containers = root.findAllContainerByPath("/group*/data*");
+            assertNotNull(containers);
+            assertTrue(containers.size() == 2);
+
+
+        }
+    }
+
+}
