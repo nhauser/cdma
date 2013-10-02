d81d0dbee5d0bf44bcb7e33fcf9aa7f256d2812d
gregory.viguier <gregory.viguier@gmail.com>
2013-10-02 08:25:52 +0000

    EdfSoleil Release 1.0.2

---
+++ java/plugins/org.cdma.plugin.soleil.edf/tags/EdfSoleil-1.0.2/src/main/java/org/cdma/plugin/soleil/edf/external/CreateVirtualItem.java
@@ -0,0 +1,58 @@
+package org.cdma.plugin.soleil.edf.external;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.cdma.arrays.DefaultArrayMatrix;
+import org.cdma.dictionary.Context;
+import org.cdma.dictionary.IPluginMethod;
+import org.cdma.exception.CDMAException;
+import org.cdma.interfaces.IAttribute;
+import org.cdma.interfaces.IContainer;
+import org.cdma.interfaces.IGroup;
+import org.cdma.plugin.soleil.edf.EdfFactory;
+import org.cdma.plugin.soleil.edf.navigation.EdfDataItem;
+import org.cdma.utils.Utilities.ModelType;
+
+public class CreateVirtualItem implements IPluginMethod {
+
+    @Override
+    public String getFactoryName() {
+        return EdfFactory.NAME;
+    }
+
+    @Override
+    public void execute(Context context) throws CDMAException {
+        List<IContainer> inList = context.getContainers();
+        List<IContainer> outList = new ArrayList<IContainer>();
+
+        EdfDataItem item;
+
+        DefaultArrayMatrix array;
+        String name;
+        for (IContainer container : inList) {
+            if (container.getModelType().equals(ModelType.Group)) {
+                name = container.getName();
+
+                array = new DefaultArrayMatrix(EdfFactory.NAME, name.toCharArray());
+                item = new EdfDataItem(EdfFactory.NAME, array);
+                item.setName(name);
+                item.setShortName(container.getShortName());
+
+                item.setParent((IGroup) container);
+                item.setCachedData(array, false);
+                for (IAttribute attr : container.getAttributeList()) {
+                    item.addOneAttribute(attr);
+                }
+                outList.add(item);
+            }
+            else {
+                outList.add(container);
+            }
+        }
+
+        // Update context
+        context.setContainers(outList);
+    }
+
+}
---
+++ java/plugins/org.cdma.plugin.soleil.edf/tags/EdfSoleil-1.0.2/src/main/java/org/cdma/plugin/soleil/edf/utils/EdfFileReader.java
@@ -0,0 +1,30 @@
+package org.cdma.plugin.soleil.edf.utils;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+public class EdfFileReader {
+
+    public EdfFileReader() {
+    }
+
+    public static ByteBuffer readAsBytes(int sizeToRead, boolean littleEndian, DataInputStream dis)
+            throws IOException {
+
+        byte[] flatByteImageValue = new byte[sizeToRead];
+        ByteBuffer result = ByteBuffer.allocate(sizeToRead);
+
+        if (littleEndian) {
+            result.order(ByteOrder.LITTLE_ENDIAN);
+        }
+
+        dis.readFully(flatByteImageValue);
+        result = result.put(flatByteImageValue);
+
+        result.rewind();
+
+        return result;
+    }
+}
