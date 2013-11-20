70f647feb8ea55922b1acaa26a79d19754782722
gregory.viguier <gregory.viguier@gmail.com>
2013-11-20 14:51:56 +0000

    Speed optimization for DataBrower and large directories

--- java/plugins/org.cdma.plugin.soleil/trunk/src/org/cdma/plugin/soleil/internal/DetectedSource.java
+++ java/plugins/org.cdma.plugin.soleil/trunk/src/org/cdma/plugin/soleil/internal/DetectedSource.java
@@ -1,4 +1,4 @@
-//******************************************************************************
+// ******************************************************************************
 // Copyright (c) 2011 Synchrotron Soleil.
 // The CDMA library is free software; you can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published by the Free
@@ -28,307 +28,299 @@ import org.cdma.utilities.configuration.ConfigDataset;
 import org.cdma.utilities.performance.Benchmarker;
 
 public class DetectedSource {
-	private static final long MIN_LAST_MODIF_TIME = 5000;
-	private static final int EXTENSION_LENGTH = 4;
-	private static final String EXTENSION = ".nxs";
-	private static final String CREATOR = "Synchrotron SOLEIL";
-	private static final String[] BEAMLINES = new String[] { "CONTACQ",
-			"AILES", "ANTARES", "CASSIOPEE", "CRISTAL", "DIFFABS", "DEIMOS",
-			"DESIRS", "DISCO", "GALAXIES", "LUCIA", "MARS", "METROLOGIE",
-			"NANOSCOPIUM", "ODE", "PLEIADES", "PROXIMA1", "PROXIMA2", "PSICHE",
-			"SAMBA", "SEXTANTS", "SIRIUS", "SIXS", "SMIS", "TEMPO", "SWING" };
+    private static final long MIN_LAST_MODIF_TIME = 5000;
+    private static final int EXTENSION_LENGTH = 4;
+    private static final String EXTENSION = ".nxs";
+    private static final String CREATOR = "Synchrotron SOLEIL";
+    private static final String[] BEAMLINES = new String[] { "CONTACQ", "AILES", "ANTARES", "CASSIOPEE", "CRISTAL",
+            "DIFFABS", "DEIMOS", "DESIRS", "DISCO", "GALAXIES", "LUCIA", "MARS", "METROLOGIE", "NANOSCOPIUM", "ODE",
+            "PLEIADES", "PROXIMA1", "PROXIMA2", "PSICHE", "SAMBA", "SEXTANTS", "SIRIUS", "SIXS", "SMIS", "TEMPO",
+            "SWING" };
 
-	public static class NeXusFilter implements FilenameFilter {
-		public boolean accept(File dir, String name) {
-			return DetectedSource.accept(name);
-		}
-	}
+    public static class NeXusFilter implements FilenameFilter {
+        public boolean accept(File dir, String name) {
+            return DetectedSource.accept(name);
+        }
+    }
 
-	private boolean mIsDataset;
-	private boolean mIsExperiment;
-	private boolean mIsBrowsable;
-	private boolean mIsProducer;
-	private boolean mIsReadable;
-	private boolean mIsFolder;
-	private boolean mInitialized;
-	private URI mURI;
-	private long mTimestamp;
+    private boolean mIsDataset;
+    private boolean mIsExperiment;
+    private boolean mIsBrowsable;
+    private boolean mIsProducer;
+    private boolean mIsReadable;
+    private boolean mIsFolder;
+    private boolean mInitialized;
+    private URI mURI;
+    private long mTimestamp;
 
-	public DetectedSource(URI uri, boolean browsable, boolean readable,
-			boolean producer, boolean experiment, boolean datasetFolder) {
-		mIsReadable = readable;
-		mIsProducer = producer;
-		mIsBrowsable = browsable;
-		mIsExperiment = experiment;
-		mIsDataset = datasetFolder;
-		mURI = uri;
-		mInitialized = true;
-		mTimestamp = getTimestamp(uri);
-	}
+    public DetectedSource(URI uri, boolean browsable, boolean readable, boolean producer, boolean experiment,
+            boolean datasetFolder) {
+        mIsReadable = readable;
+        mIsProducer = producer;
+        mIsBrowsable = browsable;
+        mIsExperiment = experiment;
+        mIsDataset = datasetFolder;
+        mURI = uri;
+        mInitialized = true;
+        mTimestamp = getTimestamp(uri);
+    }
 
-	public DetectedSource(URI uri) {
-		mURI = uri;
-		init(uri);
-	}
+    public DetectedSource(URI uri) {
+        mURI = uri;
+        init(uri);
+    }
 
-	private long getTimestamp(URI uri) {
-		long result = Integer.MIN_VALUE;
-		String path = uri.getPath();
-		if (path != null) {
-			File file = new File(path);
-			result = file.lastModified();
-		}
-		return result;
-	}
+    private long getTimestamp(URI uri) {
+        long result = Integer.MIN_VALUE;
+        String path = uri.getPath();
+        if (path != null) {
+            File file = new File(path);
+            result = file.lastModified();
+        }
+        return result;
+    }
 
-	public URI getURI() {
-		return mURI;
-	}
+    public URI getURI() {
+        return mURI;
+    }
 
-	public boolean isDatasetFolder() {
-		return mIsDataset;
-	}
+    public boolean isDatasetFolder() {
+        mIsDataset = isDatasetFolder(new File(mURI.getPath()));
+        return mIsDataset;
+    }
 
-	public boolean isExperiment() {
-		fullInit();
-		return mIsExperiment;
-	}
+    public boolean isExperiment() {
+        fullInit();
+        return mIsExperiment;
+    }
 
-	public boolean isBrowsable() {
-		fullInit();
-		return mIsBrowsable;
-	}
+    public boolean isBrowsable() {
+        fullInit();
+        return mIsBrowsable;
+    }
 
-	public boolean isProducer() {
-		fullInit();
-		return mIsProducer;
-	}
+    public boolean isProducer() {
+        fullInit();
+        return mIsProducer;
+    }
 
-	public boolean isReadable() {
-	    fullInit();
-		return mIsReadable;
-	}
+    public boolean isReadable() {
+        if (!mIsReadable) {
+            initReadable(mURI);
+        }
+        return mIsReadable;
+    }
 
-	public boolean isFolder() {
-		return mIsFolder;
-	}
+    public boolean isFolder() {
+        return mIsFolder;
+    }
 
-	public void setInitialized(boolean valueToSet) {
-		mInitialized = valueToSet;
-	}
+    public void setInitialized(boolean valueToSet) {
+        mInitialized = valueToSet;
+    }
 
-	/**
-	 * Return true if the source hasn't been modified since a while and is
-	 * considered as stable.
-	 */
-	public boolean hasChanged(URI uri) {
-		boolean result = false;
-		long currentTimestamp = getTimestamp(uri);
-		if (currentTimestamp != mTimestamp){
-			result = true;
-			
-		}
-		return result;
-	}
+    /**
+     * Return true if the source hasn't been modified since a while and is
+     * considered as stable.
+     */
+    public boolean hasChanged(URI uri) {
+        boolean result = false;
+        long currentTimestamp = getTimestamp(uri);
+        if (currentTimestamp != mTimestamp) {
+            result = true;
 
-	// ---------------------------------------------------------
-	// / private methods
-	// ---------------------------------------------------------
-	private void init(URI uri) {
-		if (uri != null) {
-			// Check if the uri is a folder
-			String path = uri.getPath();
-			if (path != null) {
-				File file = new File(path);
-				mTimestamp = file.lastModified();
-				if (file.isDirectory()) {
-					mIsDataset = isDatasetFolder(file);
-					mIsFolder = true;
-				} else {
-					mIsDataset = false;
-					mIsFolder = false;
-				}
+        }
+        return result;
+    }
 
-				// Check it is a NeXus file
-				mIsReadable = initReadable(uri);
-			} else {
-				mIsReadable = false;
-				mIsProducer = false;
-				mIsBrowsable = false;
-				mIsExperiment = false;
-				mIsDataset = false;
-				mInitialized = true;
-			}
-		}
+    // ---------------------------------------------------------
+    // / private methods
+    // ---------------------------------------------------------
+    private void init(URI uri) {
+        if (uri != null) {
+            // Check if the uri is a folder
+            String path = uri.getPath();
+            if (path != null) {
+                File file = new File(path);
+                mTimestamp = file.lastModified();
+                if (file.isDirectory()) {
 
-	}
+                    mIsFolder = true;
+                } else {
+                    mIsFolder = false;
+                }
 
-	private void fullInit() {
+                // Check it is a NeXus file
+                mIsReadable = initReadable(uri);
+            } else {
+                mIsReadable = false;
+                mIsProducer = false;
+                mIsBrowsable = false;
+                mIsExperiment = false;
+                mInitialized = true;
+            }
+        }
 
-		synchronized (this) {
-			if (!mInitialized || hasChanged(mURI)) {
+    }
 
-				mIsReadable = initReadable(mURI);
+    private void fullInit() {
 
-				// Check if we are producer of the source
-				mIsProducer = initProducer(mURI);
+        synchronized (this) {
+            if (!mInitialized || hasChanged(mURI)) {
 
-				// Check if the uri corresponds to dataset experiment
-				mIsExperiment = initExperiment(mURI);
+                // Check if we are producer of the source
+                mIsProducer = initProducer(mURI);
 
-				// Check if the URI is considered as browsable
-				mIsBrowsable = initBrowsable(mURI);
+                // Check if the uri corresponds to dataset experiment
+                mIsExperiment = initExperiment(mURI);
 
-				mInitialized = true;
-			}
-		}
-	}
+                // Check if the URI is considered as browsable
+                mIsBrowsable = initBrowsable(mURI);
 
-	private boolean initReadable(URI uri) {
+                mInitialized = true;
+            }
+        }
+    }
 
-		boolean result = false;
-		if (mIsDataset) {
-			result = true;
-		} else {
-			File file = new File(uri.getPath());
-			String name = file.getName();
+    private boolean initReadable(URI uri) {
 
-			if (file.exists() && file.length() != 0L) {
-				// Check if the URI is a NeXus file
-				if (DetectedSource.accept(name)) {
-					result = true;
-				}
-			}
-		}
+        boolean result = false;
 
-		return result;
-	}
+        File file = new File(uri.getPath());
+        String name = file.getName();
 
-	private boolean initProducer(URI uri) {
-		boolean result = false;
-		if (mIsReadable) {
-			File file = new File(uri.getPath());
-			IDataset dataset = null;
-			try {
-				// instantiate
-				dataset = NxsDataset.instanciate(file.toURI());
-				// open file
-				dataset.open();
+        if (file.exists() && file.length() != 0L) {
+            // Check if the URI is a NeXus file
+            if (DetectedSource.accept(name)) {
+                result = true;
+            }
+        }
 
-				// seek at root for 'creator' attribute
+        return result;
+    }
 
-				IGroup group = dataset.getRootGroup();
-				if (group.hasAttribute("creator", CREATOR)) {
-					result = true;
-				} else {
-					group = group.getGroup("<NXentry>");
-					if (group != null) {
-						group = group.getGroup("<NXinstrument>");
-					}
+    private boolean initProducer(URI uri) {
+        boolean result = false;
+        if (mIsReadable) {
+            File file = new File(uri.getPath());
+            IDataset dataset = null;
+            try {
+                // instantiate
+                dataset = NxsDataset.instanciate(file.toURI());
+                // open file
+                dataset.open();
 
-					if (group != null) {
-						String node = group.getShortName();
+                // seek at root for 'creator' attribute
 
-						for (String name : BEAMLINES) {
-							if (node.equalsIgnoreCase(name)) {
-								result = true;
-								break;
-							}
-						}
-					}
-				}
-				// close file
-				dataset.close();
+                IGroup group = dataset.getRootGroup();
+                if (group.hasAttribute("creator", CREATOR)) {
+                    result = true;
+                } else {
+                    group = group.getGroup("<NXentry>");
+                    if (group != null) {
+                        group = group.getGroup("<NXinstrument>");
+                    }
 
-			} catch (IOException e) {
-				// close file
-				if (dataset != null) {
-					try {
-						dataset.close();
-					} catch (IOException e1) {
-					}
-				}
-			} catch (NoResultException e) {
-				Factory.getLogger().log(Level.WARNING, e.getMessage());
-			}
-		}
-		return result;
-	}
+                    if (group != null) {
+                        String node = group.getShortName();
 
-	private boolean initExperiment(URI uri) {
-		boolean result = false;
-		// Check if the URI is a NeXus file
-		if (mIsProducer) {
-			try {
-				// Instantiate the dataset and detect its configuration
-				NxsDataset dataset = NxsDataset.instanciate(uri);
+                        for (String name : BEAMLINES) {
+                            if (node.equalsIgnoreCase(name)) {
+                                result = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                // close file
+                dataset.close();
 
-				// Interrogate the config to know the experiment path
-				ConfigDataset conf = dataset.getConfiguration();
-				if (conf != null) {
-					result = true;
-				}
-			} catch (NoResultException e) {
-			}
-		}
-		return result;
-	}
+            } catch (IOException e) {
+                // close file
+                if (dataset != null) {
+                    try {
+                        dataset.close();
+                    } catch (IOException e1) {
+                    }
+                }
+            } catch (NoResultException e) {
+                Factory.getLogger().log(Level.WARNING, e.getMessage());
+            }
+        }
+        return result;
+    }
 
-	private boolean initBrowsable(URI uri) {
-		boolean result = false;
+    private boolean initExperiment(URI uri) {
+        boolean result = false;
+        // Check if the URI is a NeXus file
+        if (mIsProducer) {
+            try {
+                // Instantiate the dataset and detect its configuration
+                NxsDataset dataset = NxsDataset.instanciate(uri);
 
-		// If experiment not browsable
-		if (!mIsExperiment) {
-			// If it is a folder containing split NeXus file (quick_exaf)
-			if (mIsFolder || mIsProducer) {
-				result = true;
-			}
-		}
-		return result;
-	}
+                // Interrogate the config to know the experiment path
+                ConfigDataset conf = dataset.getConfiguration();
+                if (conf != null) {
+                    result = true;
+                }
+            } catch (NoResultException e) {
+            }
+        }
+        return result;
+    }
 
-	/**
-	 * Return true if the given is a folder dataset.
-	 * 
-	 * @note the given file must be a folder
-	 */
-	private boolean isDatasetFolder(File file) {
-		boolean result = false;
+    private boolean initBrowsable(URI uri) {
+        boolean result = false;
 
-		NeXusFilter filter = new NeXusFilter();
-		File[] files = file.listFiles(filter);
-		if (files != null && files.length > 0) {
-			try {
-				NxsDatasource source = NxsDatasource.getInstance();
-				DetectedSource detect = source.getSource(files[0].toURI());
+        // If experiment not browsable
+        if (!mIsExperiment) {
+            // If it is a folder containing split NeXus file (quick_exaf)
+            if (mIsFolder || mIsProducer) {
+                result = true;
+            }
+        }
+        return result;
+    }
 
-				if (detect.isProducer()) {
-					IDataset dataset = new NexusDatasetImpl(files[0], false);
-					IGroup group = dataset.getRootGroup();
+    /**
+     * Return true if the given is a folder dataset.
+     * 
+     * @note the given file must be a folder
+     */
+    private boolean isDatasetFolder(File file) {
+        boolean result = false;
 
-					IContainer groups = group
-							.findContainerByPath("/<NXentry>/<NXdata>");
-					if (groups instanceof IGroup) {
-						for (IDataItem item : ((IGroup) groups)
-								.getDataItemList()) {
-							if (item.getAttribute("dataset_part") != null) {
-								result = true;
-								break;
-							}
-						}
-					}
-				}
-			} catch (NoResultException e) {
-			} catch (FileAccessException e) {
-			}
-		}
+        NeXusFilter filter = new NeXusFilter();
+        File[] files = file.listFiles(filter);
+        if (files != null && files.length > 0) {
+            try {
+                NxsDatasource source = NxsDatasource.getInstance();
+                DetectedSource detect = source.getSource(files[0].toURI());
 
-		return result;
-	}
+                if (detect.isProducer()) {
+                    IDataset dataset = new NexusDatasetImpl(files[0], false);
+                    IGroup group = dataset.getRootGroup();
 
-	private static boolean accept(String filename) {
-		int length = filename.length();
-		return (length > EXTENSION_LENGTH && filename.substring(
-				length - EXTENSION_LENGTH).equalsIgnoreCase(EXTENSION));
-	}
+                    IContainer groups = group.findContainerByPath("/<NXentry>/<NXdata>");
+                    if (groups instanceof IGroup) {
+                        for (IDataItem item : ((IGroup) groups).getDataItemList()) {
+                            if (item.getAttribute("dataset_part") != null) {
+                                result = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            } catch (NoResultException e) {
+            } catch (FileAccessException e) {
+            }
+        }
+
+        return result;
+    }
+
+    private static boolean accept(String filename) {
+        int length = filename.length();
+        return (length > EXTENSION_LENGTH && filename.substring(length - EXTENSION_LENGTH).equalsIgnoreCase(EXTENSION));
+    }
 }
