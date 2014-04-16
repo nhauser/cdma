1ca3f8ec700bbde3fa7502998e11c5ddc5ce2d62
gregory.viguier@gmail.com <gregory.viguier@gmail.com@969d0bd1-f5ba-ef64-2eb3-b0521df2be17>
2014-04-16 13:58:08 +0000

    EdfSoleil plugin Release 1.0.4

---
+++ java/plugins/org.cdma.plugin.soleil.edf/tags/EdfSoleil-1.0.4/src/main/java/org/cdma/plugin/soleil/edf/internal/DetectedSource.java
@@ -0,0 +1,87 @@
+/*******************************************************************************
+ * Copyright (c) 2008 - ANSTO/Synchrotron SOLEIL
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * 	Norman Xiong (nxi@Bragg Institute) - initial API and implementation
+ * 	Tony Lam (nxi@Bragg Institute) - initial API and implementation
+ *        Majid Ounsy (SOLEIL Synchrotron) - API v2 design and conception
+ *        Stéphane Poirier (SOLEIL Synchrotron) - API v2 design and conception
+ * 	Clement Rodriguez (ALTEN for SOLEIL Synchrotron) - API evolution
+ * 	Gregory VIGUIER (SOLEIL Synchrotron) - API evolution
+ ******************************************************************************/
+package org.cdma.plugin.soleil.edf.internal;
+
+import java.io.File;
+import java.net.URI;
+
+import org.cdma.plugin.soleil.edf.EdfDatasource.ValidURIFilter;
+
+public class DetectedSource {
+    private boolean mIsExperiment;
+    private boolean mIsBrowsable;
+    private boolean mIsProducer;
+    private boolean mIsReadable;
+    private final URI mURI;
+
+    public DetectedSource(URI uri) {
+        mURI = uri;
+        init(uri);
+    }
+
+    public URI getURI() {
+        return mURI;
+    }
+
+    public boolean isExperiment() {
+        return mIsExperiment;
+    }
+
+    public boolean isBrowsable() {
+        return mIsBrowsable;
+    }
+
+    public boolean isProducer() {
+        return mIsProducer;
+    }
+
+    public boolean isReadable() {
+        return mIsReadable;
+    }
+
+    // ---------------------------------------------------------
+    // / private methods
+    // ---------------------------------------------------------
+    private void init(URI uri) {
+        mIsExperiment = false;
+        mIsBrowsable = false;
+        mIsProducer = false;
+        mIsReadable = false;
+
+        if (uri != null) {
+            String path = uri.getPath();
+            if (path != null) {
+                File file = new File(path);
+                if (file.exists()) {
+                    mIsBrowsable = file.isDirectory();
+                    
+//                    if (mIsBrowsable){
+//                        if (EdfDatasource.findEDFFiles(file)){
+//                            mIsReadable = true;
+//                        }
+//                    }
+                    ValidURIFilter filter = new ValidURIFilter();
+                    if (filter.accept(file)) {
+                        mIsBrowsable = true;
+                        mIsReadable = true;
+                        mIsProducer = true;
+                        mIsExperiment = true;
+                    }
+                }
+            }
+        }
+    }
+}
