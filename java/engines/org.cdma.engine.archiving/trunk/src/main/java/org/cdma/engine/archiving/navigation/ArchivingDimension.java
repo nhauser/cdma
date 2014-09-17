0be7b193917240b6fc0c48746f07523e5f63d928
katy.saintin <katy.saintin@synchrotron-soleil.fr>
2014-09-17 15:35:14 +0000

    Add TimeArray object to convert String date into long timestamp

--- java/engines/org.cdma.engine.archiving/trunk/src/main/java/org/cdma/engine/archiving/navigation/ArchivingDimension.java
+++ java/engines/org.cdma.engine.archiving/trunk/src/main/java/org/cdma/engine/archiving/navigation/ArchivingDimension.java
@@ -15,102 +15,108 @@
  ******************************************************************************/
 package org.cdma.engine.archiving.navigation;
 
+import org.cdma.engine.archiving.internal.TimeArray;
 import org.cdma.exception.NotImplementedException;
 import org.cdma.exception.ShapeNotMatchException;
 import org.cdma.interfaces.IArray;
 import org.cdma.interfaces.IDimension;
 
 public class ArchivingDimension implements IDimension {
-	private IArray mArray;
-	private String mName;
-	private String mFactory;
+    private IArray mArray;
+    private String mName;
+    private final String mFactory;
 
-	public ArchivingDimension( String factory, IArray array, String name ) {
-		mFactory = factory;
-		mName = name;
-		mArray = array;
-	}
-	
-	@Override
-	public String getFactoryName() {
-		return mFactory;
-	}
+    public ArchivingDimension( String factory, IArray array, String name ) {
+        mFactory = factory;
+        mName = name;
 
-	@Override
-	public String getName() {
-		return mName;
-	}
+        if (name.contains("TIME")) {
+            mArray = new TimeArray(array);
+        } else {
+            mArray = array;
+        }
+    }
 
-	@Override
-	public int getLength() {
-		return mArray != null ? ((Long) mArray.getSize()).intValue() : -1;
-	}
+    @Override
+    public String getFactoryName() {
+        return mFactory;
+    }
 
-	@Override
-	public boolean isUnlimited() {
-		return false;
-	}
+    @Override
+    public String getName() {
+        return mName;
+    }
 
-	@Override
-	public boolean isVariableLength() {
-		return false;
-	}
+    @Override
+    public int getLength() {
+        return mArray != null ? ((Long) mArray.getSize()).intValue() : -1;
+    }
 
-	@Override
-	public boolean isShared() {
-		return false;
-	}
+    @Override
+    public boolean isUnlimited() {
+        return false;
+    }
 
-	@Override
-	public IArray getCoordinateVariable() {
-		return mArray;
-	}
+    @Override
+    public boolean isVariableLength() {
+        return false;
+    }
 
-	@Override
-	public int compareTo(Object o) {
-		int result = -1;
-		if( o instanceof IDimension ) {
-			result = mName.compareTo( ((IDimension) o).getName() );
-		}
-		return result;
-	}
+    @Override
+    public boolean isShared() {
+        return false;
+    }
 
-	@Override
-	public String writeCDL(boolean strict) {
-		throw new NotImplementedException();
-	}
+    @Override
+    public IArray getCoordinateVariable() {
+        return mArray;
+    }
 
-	@Override
-	public void setUnlimited(boolean b) {
-		throw new NotImplementedException();
+    @Override
+    public int compareTo(Object o) {
+        int result = -1;
+        if( o instanceof IDimension ) {
+            result = mName.compareTo( ((IDimension) o).getName() );
+        }
+        return result;
+    }
 
-	}
+    @Override
+    public String writeCDL(boolean strict) {
+        throw new NotImplementedException();
+    }
 
-	@Override
-	public void setVariableLength(boolean b) {
-		throw new NotImplementedException();
+    @Override
+    public void setUnlimited(boolean b) {
+        throw new NotImplementedException();
 
-	}
+    }
 
-	@Override
-	public void setShared(boolean b) {
-		throw new NotImplementedException();
+    @Override
+    public void setVariableLength(boolean b) {
+        throw new NotImplementedException();
 
-	}
+    }
 
-	@Override
-	public void setLength(int n) {
-		throw new NotImplementedException();
+    @Override
+    public void setShared(boolean b) {
+        throw new NotImplementedException();
 
-	}
+    }
 
-	@Override
-	public void setName(String name) {
-		mName = name;
-	}
+    @Override
+    public void setLength(int n) {
+        throw new NotImplementedException();
 
-	@Override
-	public void setCoordinateVariable(IArray array) throws ShapeNotMatchException {
-		mArray = array;
-	}
+    }
+
+    @Override
+    public void setName(String name) {
+        mName = name;
+    }
+
+    @Override
+    public void setCoordinateVariable(IArray array) throws ShapeNotMatchException {
+        mArray = array;
+    }
 }
