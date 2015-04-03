be4afbbb36c840ed8f45ff3caf929baeb87855c6
gregory.viguier@gmail.com <gregory.viguier@gmail.com@969d0bd1-f5ba-ef64-2eb3-b0521df2be17>
2015-04-03 12:24:03 +0000

    Add new getters for Filter Attribute

--- java/core/org.cdma.core/trunk/src/org/cdma/dictionary/filter/FilterAttribute.java
+++ java/core/org.cdma.core/trunk/src/org/cdma/dictionary/filter/FilterAttribute.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  * 	Norman Xiong (nxi@Bragg Institute) - initial API and implementation
  * 	Tony Lam (nxi@Bragg Institute) - initial API and implementation
@@ -16,10 +16,10 @@
 // ****************************************************************************
 // Copyright (c) 2010 Australian Nuclear Science and Technology Organisation.
 // All rights reserved. This program and the accompanying materials
-// are made available under the terms of the Eclipse Public License v1.0 
+// are made available under the terms of the Eclipse Public License v1.0
 // which accompanies this distribution, and is available at
 // http://www.eclipse.org/legal/epl-v10.html
-// 
+//
 // Contributors
 //    Clement Rodriguez - initial API and implementation
 // ****************************************************************************
@@ -29,37 +29,38 @@ import org.cdma.interfaces.IContainer;
 
 /**
  * @brief The FilterAttribute class is used to select a specific IContainer.
- * 
- * The IFilter implementation FilterAttribute defines a filter that will 
- * test the presence of a specific attribute having the right name and 
- * value (in String). 
- * 
+ *
+ * The IFilter implementation FilterAttribute defines a filter that will
+ * test the presence of a specific attribute having the right name and
+ * value (in String).
+ *
  * @author rodriguez
  */
 
 public class FilterAttribute implements IFilter {
     private String mName;
     private String mValue;
-    
+
     public FilterAttribute(String name, String value) {
         mName = name;
         mValue = value;
     }
-    
+
     @Override
     public boolean matches(IContainer item) {
-        return item.hasAttribute(mName, mValue);
+        boolean result = item.hasAttribute(mName, mValue);
+        return result;
     }
-    
+
     @Override
     public String toString() {
         return "FilterAttribute: " + mName + " = " + mValue;
     }
-    
+
     @Override
     public boolean equals(Object filter) {
         boolean result = false;
-        
+
         if( filter instanceof FilterAttribute ) {
             if( mName == null ) {
                 result =  ( ((FilterAttribute) filter).mName == null );
@@ -67,7 +68,7 @@ public class FilterAttribute implements IFilter {
             else {
                 result = ( mName.equals( ((FilterAttribute) filter).mName ) );
             }
-            
+
             if( result ) {
                 if( mValue == null ) {
                     result =  ( ((FilterAttribute) filter).mValue == null );
@@ -78,6 +79,14 @@ public class FilterAttribute implements IFilter {
             }
         }
         return result;
+    }
+
+    public String getName() {
+        return this.mName;
+    }
+
+    public String getValue() {
+        return this.mValue;
     }
 
 }
