3d69b03f81338b72e9464a883bbdb202ccbd4128
gregory.viguier <gregory.viguier@gmail.com>
2015-05-15 14:29:09 +0000

    Bugfixes and code clean up

--- java/engines/org.cdma.engine.hdf/trunk/src/main/java/org/cdma/engine/hdf/utils/HdfNode.java
+++ java/engines/org.cdma.engine.hdf/trunk/src/main/java/org/cdma/engine/hdf/utils/HdfNode.java
@@ -15,36 +15,23 @@
  ******************************************************************************/
 package org.cdma.engine.hdf.utils;
 
-import org.cdma.interfaces.IAttribute;
 import org.cdma.interfaces.IContainer;
 import org.cdma.interfaces.INode;
 
 public class HdfNode implements INode {
     // Private definitions
-    private static final String ATTRIBUTE_SEPARATOR_START = "<";
-    private static final String ATTRIBUTE_SEPARATOR_START2 = "{";
-    // TODO
-    private static final String SELECTED_ATTRIBUTE_TO_LOAD = "NX_class";
+    public static final String ATTRIBUTE_SEPARATOR_START = "<";
+    public static final String ATTRIBUTE_SEPARATOR_START2 = "{";
 
-    private final String name;
+    protected final String name;
     private boolean isGroup;
-    private String attribute = "";
 
-    public HdfNode(final String name, final String attribute) {
+    public HdfNode(final String name) {
         this.name = name;
-        this.attribute = attribute;
     }
 
-    public HdfNode(final String fullName) {
-        this(extractName(fullName), extractClass(fullName));
-    }
-
-    public HdfNode(final IContainer container) {
+    public HdfNode(IContainer container) {
         this.name = container.getShortName();
-        IAttribute attribute = container.getAttribute(SELECTED_ATTRIBUTE_TO_LOAD);
-        if (attribute != null) {
-            this.attribute = attribute.getStringValue();
-        }
     }
 
     @Override
@@ -58,29 +45,22 @@ public class HdfNode implements INode {
     }
 
     @Override
-    public String getAttribute() {
-        return attribute;
-    }
-
-    @Override
     public boolean matchesNode(final INode node) {
-        boolean classMatch, nameMatch;
-        classMatch = "".equals(node.getAttribute()) || node.getAttribute().equalsIgnoreCase(this.getAttribute());
+        boolean nameMatch;
         nameMatch = "".equals(node.getNodeName()) || this.getNodeName().equalsIgnoreCase(node.getNodeName());
 
-        return (classMatch && nameMatch);
+        return (nameMatch);
     }
 
     @Override
     public boolean matchesPartNode(final INode node) {
-        boolean classMatch = false, nameMatch = false;
+        boolean nameMatch = false;
         if (node != null) {
-            classMatch = "".equals(node.getAttribute()) || node.getAttribute().equalsIgnoreCase(this.getAttribute());
             nameMatch = "".equals(node.getNodeName())
                     || this.getNodeName().toLowerCase().replace("*", ".*")
                             .matches(node.getNodeName().toLowerCase().replace("*", ".*"));
         }
-        return (classMatch && nameMatch);
+        return (nameMatch);
     }
 
     @Override
@@ -102,18 +82,6 @@ public class HdfNode implements INode {
         iPosClassSep = iPosClassSep < 0 ? sNodeName.length() : iPosClassSep;
         tmpNodeName = sNodeName.substring(0, iPosClassSep);
         return tmpNodeName;
-    }
-
-    public static String extractClass(final String sNodeName) {
-        int iPosClassSep;
-        String tmpClassName = "";
-        iPosClassSep = sNodeName.indexOf(ATTRIBUTE_SEPARATOR_START);
-        if (iPosClassSep < 0)
-            iPosClassSep = sNodeName.indexOf(ATTRIBUTE_SEPARATOR_START2);
-        iPosClassSep = iPosClassSep < 0 ? sNodeName.length() : iPosClassSep;
-        tmpClassName = iPosClassSep < sNodeName.length() ? sNodeName
-                .substring(iPosClassSep + 1, sNodeName.length() - 1) : "";
-        return tmpClassName;
     }
 
 }
