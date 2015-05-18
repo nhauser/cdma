16745b91aab14d583921ed7b0bf2b9ee5e50f26b
gregory.viguier@gmail.com <gregory.viguier@gmail.com@969d0bd1-f5ba-ef64-2eb3-b0521df2be17>
2015-05-18 08:22:28 +0000

    Code style format

--- java/core/org.cdma.core/trunk/src/org/cdma/utils/DefaultNode.java
+++ java/core/org.cdma.core/trunk/src/org/cdma/utils/DefaultNode.java
@@ -6,12 +6,12 @@
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- * 	Norman Xiong (nxi@Bragg Institute) - initial API and implementation
- * 	Tony Lam (nxi@Bragg Institute) - initial API and implementation
- *        Majid Ounsy (SOLEIL Synchrotron) - API v2 design and conception
- *        Stéphane Poirier (SOLEIL Synchrotron) - API v2 design and conception
- * 	Clement Rodriguez (ALTEN for SOLEIL Synchrotron) - API evolution
- * 	Gregory VIGUIER (SOLEIL Synchrotron) - API evolution
+ * Norman Xiong (nxi@Bragg Institute) - initial API and implementation
+ * Tony Lam (nxi@Bragg Institute) - initial API and implementation
+ * Majid Ounsy (SOLEIL Synchrotron) - API v2 design and conception
+ * Stéphane Poirier (SOLEIL Synchrotron) - API v2 design and conception
+ * Clement Rodriguez (ALTEN for SOLEIL Synchrotron) - API evolution
+ * Gregory VIGUIER (SOLEIL Synchrotron) - API evolution
  ******************************************************************************/
 package org.cdma.utils;
 
@@ -53,8 +53,7 @@ public class DefaultNode implements INode {
     public boolean matchesNode(INode node) {
         boolean nameMatch;
 
-        nameMatch = "".equals(node.getNodeName())
-                || this.getNodeName().equalsIgnoreCase(node.getNodeName());
+        nameMatch = "".equals(node.getNodeName()) || this.getNodeName().equalsIgnoreCase(node.getNodeName());
 
         return nameMatch;
     }
@@ -65,7 +64,7 @@ public class DefaultNode implements INode {
 
         nameMatch = "".equals(node.getNodeName())
                 || this.getNodeName().toLowerCase().replace("*", ".*")
-                .matches(node.getNodeName().toLowerCase().replace("*", ".*"));
+                        .matches(node.getNodeName().toLowerCase().replace("*", ".*"));
         return nameMatch;
     }
 
@@ -79,7 +78,6 @@ public class DefaultNode implements INode {
         return name;
     }
 
-    @Override
     public String getAttribute() {
         return attribute;
     }
