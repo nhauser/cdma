c4035d93f524f49d93733d38c8a8c8df0cc87582
gregory.viguier@gmail.com <gregory.viguier@gmail.com@969d0bd1-f5ba-ef64-2eb3-b0521df2be17>
2015-05-18 09:42:27 +0000

    NexusSoleil 2.0.1

---
+++ java/plugins/org.cdma.plugin.soleil.nexus/tags/NexusSoleil-2.0.1/src/main/java/org/cdma/plugin/soleil/nexus/NxsFactory.java
@@ -0,0 +1,317 @@
+/*******************************************************************************
+ * Copyright (c) 2008 - ANSTO/Synchrotron SOLEIL
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * 	Norman Xiong (nxi@Bragg Institute) - initial API and implementation
+ * 	Tony Lam (nxi@Bragg Institute) - initial API and implementation
+ *        Majid Ounsy (SOLEIL Synchrotron) - API v2 design and conception
+ *        Stéphane Poirier (SOLEIL Synchrotron) - API v2 design and conception
+ * 	Clement Rodriguez (ALTEN for SOLEIL Synchrotron) - API evolution
+ * 	Gregory VIGUIER (SOLEIL Synchrotron) - API evolution
+ ******************************************************************************/
+package org.cdma.plugin.soleil.nexus;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.net.URI;
+import java.util.logging.Level;
+
+import ncsa.hdf.object.h5.H5ScalarDS;
+
+import org.cdma.Factory;
+import org.cdma.IFactory;
+import org.cdma.dictionary.Key;
+import org.cdma.dictionary.LogicalGroup;
+import org.cdma.dictionary.Path;
+import org.cdma.engine.hdf.navigation.HdfAttribute;
+import org.cdma.engine.hdf.navigation.HdfDataset;
+import org.cdma.engine.hdf.utils.HdfObjectUtils;
+import org.cdma.exception.FileAccessException;
+import org.cdma.exception.InvalidArrayTypeException;
+import org.cdma.exception.NoResultException;
+import org.cdma.exception.NotImplementedException;
+import org.cdma.interfaces.IArray;
+import org.cdma.interfaces.IAttribute;
+import org.cdma.interfaces.IDataItem;
+import org.cdma.interfaces.IDataset;
+import org.cdma.interfaces.IDatasource;
+import org.cdma.interfaces.IDictionary;
+import org.cdma.interfaces.IGroup;
+import org.cdma.interfaces.IKey;
+import org.cdma.math.IArrayMath;
+import org.cdma.plugin.soleil.nexus.array.NxsArray;
+import org.cdma.plugin.soleil.nexus.dictionary.NxsLogicalGroup;
+import org.cdma.plugin.soleil.nexus.navigation.NxsDataset;
+import org.cdma.plugin.soleil.nexus.navigation.NxsGroup;
+import org.cdma.plugin.soleil.nexus.utils.NxsArrayMath;
+import org.cdma.plugin.soleil.nexus.utils.NxsPath;
+
+public final class NxsFactory implements IFactory {
+    private static NxsFactory factory;
+    private static NxsDatasource detector;
+    public static final String NAME = "SoleilNeXus2";
+    public static final String LABEL = "SOLEIL's NeXus plug-in v2";
+    public static final String DEBUG_INF = "CDMA_DEBUG";
+    public static final String CONFIG_FILE = "cdma_nexussoleil_config.xml";
+    private static final String CDMA_VERSION = "3_2_0";
+    private static final String PLUG_VERSION = "2.0.0";
+    private static final String DESC = "This plug-in manages NeXus data files (having 'nxs' for extension).";
+
+    public NxsFactory() {
+    }
+
+    public static NxsFactory getInstance() {
+        synchronized (NxsFactory.class) {
+            if (factory == null) {
+                factory = new NxsFactory();
+                detector = NxsDatasource.getInstance();
+            }
+        }
+        return factory;
+    }
+
+    @Override
+    public IArray createArray(final Class<?> clazz, final int[] shape) {
+        Object o = java.lang.reflect.Array.newInstance(clazz, shape);
+        IArray result = null;
+        try {
+            result = new NxsArray(o, shape);
+        } catch (InvalidArrayTypeException e) {
+            Factory.getLogger().log(Level.SEVERE, "Unable to initialize data!", e);
+        }
+        return result;
+    }
+
+    @Override
+    public IArray createArray(final Class<?> clazz, final int[] shape, final Object storage) {
+        IArray result = null;
+        if (storage instanceof IArray[]) {
+            result = new NxsArray((IArray[]) storage);
+        } else if (storage instanceof H5ScalarDS) {
+            try {
+                result = new NxsArray(((H5ScalarDS) storage).getData(), shape);
+            } catch (Exception e) {
+                Factory.getLogger().log(Level.SEVERE, "Unable to initialize data!", e);
+            }
+
+        } else {
+            try {
+                result = new NxsArray(storage, shape);
+            } catch (InvalidArrayTypeException e) {
+                Factory.getLogger().log(Level.SEVERE, "Unable to initialize data!", e);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public IArray createArray(final Object javaArray) {
+        IArray result = null;
+        // [ANSTO][Tony][2011-08-31] testing isArray may be slow
+        // [SOLEIL][clement][2012-04-18] as the supported array is a primitive type the "instanceof" won't be correct
+        if (javaArray != null && javaArray.getClass().isArray()) {
+            int size = Array.getLength(javaArray);
+            try {
+                result = new NxsArray(javaArray, new int[] { size });
+            } catch (InvalidArrayTypeException e) {
+                Factory.getLogger().log(Level.SEVERE, "Unable to initialize data!", e);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public IArray createArrayNoCopy(final Object array) {
+        IArray result = null;
+        if (array instanceof IArray[]) {
+            result = new NxsArray((IArray[]) array);
+        } else if (array instanceof H5ScalarDS) {
+
+            H5ScalarDS hdfItem = (H5ScalarDS) array;
+            try {
+                result = new NxsArray(hdfItem.getData(), HdfObjectUtils.convertLongToInt(hdfItem.getDims()));
+            } catch (Exception e) {
+                Factory.getLogger().log(Level.SEVERE, "Unable to initialize data!", e);
+            }
+        } else {
+            int size = Array.getLength(array);
+            try {
+                result = new NxsArray(array, new int[] { size });
+            } catch (Exception e) {
+                result = null;
+            }
+
+        }
+        return result;
+    }
+
+    @Override
+    public IAttribute createAttribute(final String name, final Object value) {
+        return new HdfAttribute(NAME, name, value);
+    }
+
+    @Override
+    public IDataItem createDataItem(final IGroup parent, final String shortName, final IArray array) throws InvalidArrayTypeException {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public IDataset createDatasetInstance(final URI uri) throws Exception {
+        return NxsDataset.instanciate(uri);
+    }
+
+    @Override
+    public IArray createDoubleArray(final double[] javaArray) {
+        IArray result = null;
+        try {
+            int[] shape = new int[1];
+            shape[0] = javaArray.length;
+            result = new NxsArray(javaArray, shape);
+        } catch (InvalidArrayTypeException e) {
+            Factory.getLogger().log(Level.SEVERE, "Unable to initialize data!", e);
+        }
+        return result;
+    }
+
+    @Override
+    public IArray createDoubleArray(final double[] javaArray, final int[] shape) {
+
+        IArray result = null;
+        try {
+            result = new NxsArray(javaArray, shape);
+        } catch (InvalidArrayTypeException e) {
+            Factory.getLogger().log(Level.SEVERE, "Unable to initialize data!", e);
+        }
+        return result;
+    }
+
+    @Override
+    public IDataset createEmptyDatasetInstance() throws IOException {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public IGroup createGroup(final IGroup parent, final String shortName) {
+        String path_val = parent.getLocation();
+        // PathGroup path = new PathGroup(NxsPath.splitStringPath(path_val));
+        NxsPath nxsPath = new NxsPath((NxsPath.splitStringToNode(path_val)));
+        NxsGroup group = new NxsGroup(parent, nxsPath, (NxsDataset) parent.getDataset());
+
+        return group;
+    }
+
+    @Override
+    public IGroup createGroup(final String shortName) throws IOException {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public IArray createStringArray(final String string) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public String getPluginLabel() {
+        return LABEL;
+    }
+
+    @Override
+    public IDatasource getPluginURIDetector() {
+        synchronized (NxsDatasource.class) {
+            if (detector == null) {
+                detector = NxsDatasource.getInstance();
+            }
+        }
+        return detector;
+    }
+
+    @Override
+    public IDataset openDataset(final URI uri) throws FileAccessException {
+        IDataset ds = null;
+        try {
+            ds = NxsDataset.instanciate(uri);
+        } catch (NoResultException e) {
+            throw new FileAccessException(e);
+        }
+        return ds;
+    }
+
+    @Override
+    public IKey createKey(final String keyName) {
+        return new Key(this, keyName);
+    }
+
+    @Override
+    public LogicalGroup createLogicalGroup(final IDataset dataset, final IKey key) {
+        return new NxsLogicalGroup(dataset, new Key(this, key.getName()));
+    }
+
+    @Override
+    public Path createPath(final String path) {
+        return new Path(this, path);
+    }
+
+    @Deprecated
+    @Override
+    public IDictionary openDictionary(final URI uri) throws FileAccessException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Deprecated
+    @Override
+    public IDictionary openDictionary(final String filepath) throws FileAccessException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Deprecated
+    @Override
+    public IDictionary createDictionary() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static IArrayMath createArrayMath(final NxsArray array) {
+        return new NxsArrayMath(array);
+    }
+
+    @Override
+    public String getPluginVersion() {
+        return PLUG_VERSION;
+    }
+
+    @Override
+    public String getCDMAVersion() {
+        return CDMA_VERSION;
+    }
+
+    @Override
+    public String getPluginDescription() {
+        return NxsFactory.DESC;
+    }
+
+    @Override
+    public void processPostRecording() {
+        synchronized (NxsFactory.class) {
+            boolean checkHdfAPI = HdfDataset.checkHdfAPI();
+            if (!checkHdfAPI) {
+                Factory.getManager().unregisterFactory(NAME);
+                factory = null;
+            }
+        }
+    }
+
+    @Override
+    public boolean isLogicalModeAvailable() {
+        String dictPath = Factory.getDictionariesFolder();
+        return (dictPath != null && !dictPath.isEmpty());
+    }
+
+}
---
+++ java/plugins/org.cdma.plugin.soleil.nexus/tags/NexusSoleil-2.0.1/src/main/java/org/cdma/plugin/soleil/nexus/internal/DetectedSource.java
@@ -0,0 +1,342 @@
+/*******************************************************************************
+ * Copyright (c) 2008 - ANSTO/Synchrotron SOLEIL
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Norman Xiong (nxi@Bragg Institute) - initial API and implementation
+ * Tony Lam (nxi@Bragg Institute) - initial API and implementation
+ * Majid Ounsy (SOLEIL Synchrotron) - API v2 design and conception
+ * Stéphane Poirier (SOLEIL Synchrotron) - API v2 design and conception
+ * Clement Rodriguez (ALTEN for SOLEIL Synchrotron) - API evolution
+ * Gregory VIGUIER (SOLEIL Synchrotron) - API evolution
+ ******************************************************************************/
+package org.cdma.plugin.soleil.nexus.internal;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.net.URI;
+
+import org.cdma.exception.FileAccessException;
+import org.cdma.exception.NoResultException;
+import org.cdma.interfaces.IContainer;
+import org.cdma.interfaces.IDataItem;
+import org.cdma.interfaces.IDataset;
+import org.cdma.interfaces.IGroup;
+import org.cdma.plugin.soleil.nexus.NxsDatasource;
+import org.cdma.plugin.soleil.nexus.navigation.NxsDataset;
+import org.cdma.utilities.configuration.ConfigDataset;
+
+public class DetectedSource {
+    private static final long MIN_LAST_MODIF_TIME = 5000;
+    private static final int EXTENSION_LENGTH = 4;
+    private static final String EXTENSION[] = new String[] { "nxs", "hdf", "h4", "hdf4", "he4", "h5", "hdf5", "he5" };
+    private static final String CREATOR = "Synchrotron SOLEIL";
+    private static final String[] BEAMLINES = new String[] { "CONTACQ", "AILES", "ANTARES", "CASSIOPEE", "CRISTAL",
+        "DIFFABS", "DEIMOS", "DESIRS", "DISCO", "GALAXIES", "LUCIA", "MARS", "METROLOGIE", "NANOSCOPIUM", "ODE",
+        "PLEIADES", "PROXIMA1", "PROXIMA2", "PSICHE", "SAMBA", "SEXTANTS", "SIRIUS", "SIXS", "SMIS", "TEMPO",
+        "SWING", "ROCK", "PUMA", "ANATOMIX", "HERMES" };
+
+    public static class NeXusFilter implements FilenameFilter {
+        @Override
+        public boolean accept(final File dir, final String name) {
+            return DetectedSource.accept(name);
+        }
+    }
+
+    private boolean mIsDataset;
+    private boolean mIsExperiment;
+    private boolean mIsBrowsable;
+    private boolean mIsProducer;
+    private boolean mIsReadable;
+    private boolean mIsFolder;
+    private boolean mInitialized;
+    private final URI mURI;
+    private long mTimestamp;
+
+    public DetectedSource(final URI uri, final boolean browsable, final boolean readable, final boolean producer, final boolean experiment,
+            final boolean datasetFolder) {
+        mIsReadable = readable;
+        mIsProducer = producer;
+        mIsBrowsable = browsable;
+        mIsExperiment = experiment;
+        mIsDataset = datasetFolder;
+        mURI = uri;
+        mInitialized = true;
+        mTimestamp = getTimestamp(uri);
+    }
+
+    public DetectedSource(final URI uri) {
+        mURI = uri;
+        init(uri);
+    }
+
+    private long getTimestamp(final URI uri) {
+        long result = Integer.MIN_VALUE;
+        String path = uri.getPath();
+        if (path != null) {
+            File file = new File(path);
+            result = file.lastModified();
+        }
+        return result;
+    }
+
+    public URI getURI() {
+        return mURI;
+    }
+
+    public boolean isDatasetFolder() {
+        mIsDataset = isDatasetFolder(new File(mURI.getPath()));
+        return mIsDataset;
+    }
+
+    public boolean isExperiment() {
+        // For NeXus SOLEIL: experience implies a fragment in URI
+        if (mURI.getFragment() != null) {
+            fullInit();
+        }
+        return mIsExperiment;
+    }
+
+    public boolean isBrowsable() {
+        fullInit();
+        return mIsBrowsable;
+    }
+
+    public boolean isProducer() {
+        return true;
+        // fullInit();
+        // return mIsProducer;
+    }
+
+    public boolean isReadable() {
+        if (!mIsReadable) {
+            mIsReadable = initReadable(mURI);
+        }
+        return mIsReadable;
+    }
+
+    public boolean isFolder() {
+        return mIsFolder;
+    }
+
+    public void setInitialized(final boolean valueToSet) {
+        mInitialized = valueToSet;
+    }
+
+    /**
+     * Return true if the source hasn't been modified since a while and is
+     * considered as stable.
+     */
+    public boolean hasChanged(final URI uri) {
+        boolean result = false;
+        long currentTimestamp = getTimestamp(uri);
+        if (currentTimestamp != mTimestamp) {
+            result = true;
+        }
+        return result;
+    }
+
+    // ---------------------------------------------------------
+    // / private methods
+    // ---------------------------------------------------------
+    private void init(final URI uri) {
+        if (uri != null) {
+            // Check if the uri is a folder
+            String path = uri.getPath();
+            if (path != null) {
+                File file = new File(path);
+                mTimestamp = file.lastModified();
+                if (file.isDirectory()) {
+
+                    mIsFolder = true;
+                } else {
+                    mIsFolder = false;
+                }
+
+                // Check it is a NeXus file
+                mIsReadable = initReadable(uri);
+            } else {
+                mIsReadable = false;
+                mIsProducer = false;
+                mIsBrowsable = false;
+                mIsExperiment = false;
+                mInitialized = true;
+            }
+        }
+    }
+
+    private void fullInit() {
+        synchronized (this) {
+            if (!mInitialized || hasChanged(mURI)) {
+                // Check if we are producer of the source
+                mIsProducer = initProducer(mURI);
+
+                // Check if the uri corresponds to dataset experiment
+                mIsExperiment = initExperiment(mURI);
+
+                // Check if the URI is considered as browsable
+                mIsBrowsable = initBrowsable(mURI);
+
+                mInitialized = true;
+            }
+        }
+    }
+
+    private boolean initReadable(final URI uri) {
+        boolean result = false;
+
+        File file = new File(uri.getPath());
+        String name = file.getName();
+
+        if (file.exists()) {
+            // Check if the URI is a NeXus file
+            if (DetectedSource.accept(name)) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    private boolean initProducer(final URI uri) {
+
+        boolean result = false;
+        if (mIsReadable) {
+            result = true;
+        }
+
+        // File file = new File(uri.getPath());
+        // IDataset dataset = null;
+        //
+        // try {
+        // // instantiate
+        // dataset = NxsDataset.instanciate(file.toURI());
+        // // open file
+        // dataset.open();
+        //
+        // // seek at root for 'creator' attribute
+        //
+        // IGroup group = dataset.getRootGroup();
+        // if (group.hasAttribute("creator", CREATOR)) {
+        // result = true;
+        // } else {
+        // group = group.getGroup("<NXentry>");
+        // if (group != null) {
+        // group = group.getGroup("<NXinstrument>");
+        // }
+        //
+        // if (group != null) {
+        // String node = group.getShortName();
+        //
+        // for (String name : BEAMLINES) {
+        // if (node.equalsIgnoreCase(name)) {
+        // result = true;
+        // break;
+        // }
+        // }
+        // }
+        // }
+        // // close file
+        // dataset.close();
+        //
+        // } catch (IOException e) {
+        // // close file
+        // if (dataset != null) {
+        // try {
+        // dataset.close();
+        // } catch (IOException e1) {
+        // }
+        // }
+        // } catch (NoResultException e) {
+        // Factory.getLogger().log(Level.WARNING, e.getMessage());
+        // }
+        // }
+        return result;
+    }
+
+    private boolean initExperiment(final URI uri) {
+        boolean result = false;
+        // Check if the URI is a NeXus file
+        if (mIsProducer) {
+            try {
+                // Instantiate the dataset and detect its configuration
+                NxsDataset dataset = NxsDataset.instanciate(uri);
+
+                // Interrogate the config to know the experiment path
+                ConfigDataset conf = dataset.getConfiguration();
+                if (conf != null) {
+                    result = true;
+                }
+            } catch (NoResultException e) {
+            }
+        }
+        return result;
+    }
+
+    private boolean initBrowsable(final URI uri) {
+        boolean result = false;
+
+        // If experiment not browsable
+        if (!mIsExperiment) {
+            // If it is a folder containing split NeXus file (quick_exaf)
+            if (mIsFolder || mIsProducer) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Return true if the given is a folder dataset.
+     *
+     * @note the given file must be a folder
+     */
+    private boolean isDatasetFolder(final File file) {
+        boolean result = false;
+
+        NeXusFilter filter = new NeXusFilter();
+        File[] files = file.listFiles(filter);
+        if (files != null && files.length > 0) {
+            try {
+                NxsDatasource source = NxsDatasource.getInstance();
+                DetectedSource detect = source.getSource(files[0].toURI());
+
+                if (detect.isProducer()) {
+                    IDataset dataset = new NexusDatasetImpl(files[0], false);
+                    IGroup group = dataset.getRootGroup();
+
+                    IContainer groups = group.findContainerByPath("/<NXentry>/<NXdata>");
+                    if (groups instanceof IGroup) {
+                        for (IDataItem item : ((IGroup) groups).getDataItemList()) {
+                            if (item.getAttribute("dataset_part") != null) {
+                                result = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            } catch (NoResultException e) {
+            } catch (FileAccessException e) {
+            } catch (Exception e) {
+            }
+        }
+        return result;
+    }
+
+    private static boolean accept(final String filename) {
+        boolean result = false;
+        if (filename != null) {
+            int length = filename.length();
+            for (String extension : EXTENSION) {
+                if (length >= extension.length() && filename.endsWith(extension)) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
