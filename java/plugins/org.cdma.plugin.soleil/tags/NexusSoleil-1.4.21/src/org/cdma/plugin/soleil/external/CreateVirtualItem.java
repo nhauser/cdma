70debc7f9f79cbe0c3b80d8833332d05743874b2
qa-soleil <qa-soleil@synchrotron-soleil.fr>
2015-06-22 14:31:42 +0000

    release

---
+++ java/plugins/org.cdma.plugin.soleil/tags/NexusSoleil-1.4.21/src/org/cdma/plugin/soleil/external/CreateVirtualItem.java
@@ -0,0 +1,108 @@
+/*******************************************************************************
+ * Copyright (c) 2008 - ANSTO/Synchrotron SOLEIL
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Norman Xiong (nxi@Bragg Institute) - initial API and implementation
+ * Tony Lam (nxi@Bragg Institute) - initial API and implementation
+ * Majid Ounsy (SOLEIL Synchrotron) - API v2 design and conception
+ * Stéphane Poirier (SOLEIL Synchrotron) - API v2 design and conception
+ * Clement Rodriguez (ALTEN for SOLEIL Synchrotron) - API evolution
+ * Gregory VIGUIER (SOLEIL Synchrotron) - API evolution
+ ******************************************************************************/
+package org.cdma.plugin.soleil.external;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.cdma.dictionary.Context;
+import org.cdma.dictionary.IPluginMethod;
+import org.cdma.exception.CDMAException;
+import org.cdma.interfaces.IAttribute;
+import org.cdma.interfaces.IContainer;
+import org.cdma.interfaces.IGroup;
+import org.cdma.plugin.soleil.NxsFactory;
+import org.cdma.plugin.soleil.array.NxsArray;
+import org.cdma.plugin.soleil.navigation.NxsDataItem;
+import org.cdma.utils.Utilities.ModelType;
+
+import fr.soleil.nexus.PathNexus;
+
+/**
+ * Create a list of IDataItem that are empty from a IGroup list. Created items have no data linked.
+ *
+ * @param context
+ * @throws CDMAException
+ */
+
+public class CreateVirtualItem implements IPluginMethod {
+
+    @Override
+    public String getFactoryName() {
+        return NxsFactory.NAME;
+    }
+
+    @Override
+    public void execute(final Context context) throws CDMAException {
+        List<IContainer> inList = context.getContainers();
+        List<IContainer> outList = new ArrayList<IContainer>();
+
+        NxsDataItem item;
+        PathNexus path;
+        NxsArray array;
+        String name;
+        String parameter = null;
+
+        // Backward compatibility
+        Object[] parameters = context.getParams();
+        if (parameters != null) {
+            if (parameters.length > 0) {
+                Object parameterAsObject = parameters[0];
+                if (parameterAsObject instanceof String) {
+                    parameter = (String) parameterAsObject;
+                }
+            }
+        }
+        for (IContainer container : inList) {
+            if (container.getModelType().equals(ModelType.Group)) {
+                name = container.getName();
+                path = new PathNexus(PathNexus.splitStringToNode(container.getLocation()));
+                item = new NxsDataItem();
+
+                // Backward compatibility
+                if (parameter == null) {
+                    parameter = name;
+                }
+                Object arrayValue = parameter.toCharArray();
+                int size = parameter.length();
+
+                try {
+                    arrayValue = new Double[] { Double.valueOf(parameter) };
+                    size = 1;
+                } catch (NumberFormatException nfe) {
+
+                }
+                array = new NxsArray(arrayValue, new int[] { size });
+
+                item.setName(name);
+                item.setShortName(container.getShortName());
+                item.setDataset(container.getDataset());
+                item.getNexusItems()[0].setPath(path);
+                item.setParent((IGroup) container);
+                item.setCachedData(array, false);
+                for (IAttribute attr : container.getAttributeList()) {
+                    item.addOneAttribute(attr);
+                }
+                outList.add(item);
+            } else {
+                outList.add(container);
+            }
+        }
+
+        // Update context
+        context.setContainers(outList);
+    }
+}
