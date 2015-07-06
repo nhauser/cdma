c154809dadd70ba04dcf555efcf1c2552a93cee8
qa-soleil <qa-soleil@synchrotron-soleil.fr>
2015-07-06 13:43:36 +0000

    release

---
+++ java/core/org.cdma.core/tags/Core-3.2.13/doc/pluginAPI/html/search/files_64.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['datatype_2ejava',['DataType.java',['../_data_type_8java.html',1,'']]],
+  ['dimensionnotsupportedexception_2ejava',['DimensionNotSupportedException.java',['../_dimension_not_supported_exception_8java.html',1,'']]],
+  ['dividebyzeroexception_2ejava',['DivideByZeroException.java',['../_divide_by_zero_exception_8java.html',1,'']]],
+  ['package_2dinfo_2ejava',['package-info.java',['../dictionary_2impl_2package-info_8java.html',1,'']]],
+  ['package_2dinfo_2ejava',['package-info.java',['../dictionary_2package-info_8java.html',1,'']]]
+];
---
+++ java/core/org.cdma.core/tags/Core-3.2.13/src/org/cdma/arrays/DefaultArray.java
@@ -0,0 +1,306 @@
+/*******************************************************************************
+ * Copyright (c) 2008 - ANSTO/Synchrotron SOLEIL
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * 	Norman Xiong (nxi@Bragg Institute) - initial API and implementation
+ * 	Tony Lam (nxi@Bragg Institute) - initial API and implementation
+ *        Majid Ounsy (SOLEIL Synchrotron) - API v2 design and conception
+ *        Stéphane Poirier (SOLEIL Synchrotron) - API v2 design and conception
+ * 	Clement Rodriguez (ALTEN for SOLEIL Synchrotron) - API evolution
+ * 	Gregory VIGUIER (SOLEIL Synchrotron) - API evolution
+ ******************************************************************************/
+// ****************************************************************************
+// Copyright (c) 2008 Australian Nuclear Science and Technology Organisation.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+//
+// Contributors:
+//    Clément Rodriguez (clement.rodriguez@synchrotron-soleil.fr)
+// ****************************************************************************
+package org.cdma.arrays;
+
+import org.cdma.Factory;
+import org.cdma.exception.InvalidArrayTypeException;
+import org.cdma.exception.InvalidRangeException;
+import org.cdma.exception.NotImplementedException;
+import org.cdma.exception.ShapeNotMatchException;
+import org.cdma.interfaces.IArray;
+import org.cdma.interfaces.IArrayIterator;
+import org.cdma.interfaces.IIndex;
+import org.cdma.math.IArrayMath;
+import org.cdma.utils.ArrayTools;
+import org.cdma.utils.IArrayUtils;
+
+/**
+ * This helper class aims to provide a simple way to instantiate IArray implementation
+ * which have a standard way of managing their backing array storage.
+ * 
+ * Those implementations can manage:
+ *  - java inline arrays (a single raw of data that can represent
+ * multidimensional array i.e an image for example).
+ *  - java multi-dimensional arrays which the shape can be fully described by a
+ *  simple int[]. It means no raged array, only 'squared' matrices.
+ * 
+ * @author rodriguez
+ *
+ */
+
+public abstract class DefaultArray implements IArray {
+    /**
+     * Instantiate a default IArray implementation having the given storage inline
+     * storage and the given shape
+     * 
+     * @param factory: name of the plug-in
+     * @param storage: inline storage of memory
+     * @param shape: how this inline storage should be interpreted (1D, 2D,..., ND)
+     * @return a default array implementation
+     * @throws InvalidArrayTypeException
+     */
+    static public IArray instantiateDefaultArray(final String factory, final Object storage, final int[] shape ) throws InvalidArrayTypeException {
+        IArray result = null;
+        int[] storageShape = ArrayTools.detectShape(storage);
+        if( storageShape.length == 1 ) {
+            result = new DefaultArrayInline(factory, storage, shape);
+        }
+        else {
+            result = new DefaultArrayMatrix(factory, storage);
+        }
+        return result;
+    }
+
+    /**
+     * Instantiate a default IArray implementation having the given multi-dimensional
+     * storage. The shape will be determined according the storage.
+     * 
+     * @param factory: name of the plug-in
+     * @param storage: storage of memory
+     * @return a default array implementation
+     * @throws InvalidArrayTypeException
+     */
+    static public IArray instantiateDefaultArray(final String factory, final Object storage ) throws InvalidArrayTypeException {
+        IArray result = null;
+        int[] shape = ArrayTools.detectShape(storage);
+        switch( shape.length ) {
+            case 0:
+                Object array = java.lang.reflect.Array.newInstance(storage.getClass(), 1);
+                java.lang.reflect.Array.set(array, 0, storage);
+                result = new DefaultArrayInline(factory, array, shape);
+                break;
+            case 1:
+                result = new DefaultArrayInline(factory, storage, shape);
+                break;
+            default:
+                result = new DefaultArrayMatrix(factory, storage);
+                break;
+        }
+        return result;
+    }
+
+    // private members
+    private DefaultIndex mIndex;    // IIndex corresponding to this IArray (dimension sizes defining the viewable part of the array)
+    private boolean     mIsDirty;  // Is the array synchronized with the handled file
+    private String mFactory; // Name of the instantiating factory
+    private Class<?> mClazz; // Type of the array's element
+    private boolean     mLock;     // Is the array locked
+
+    protected DefaultArray( final String factory, final Class<?> clazz, final int[] shape ) throws InvalidArrayTypeException {
+        mLock    = false;
+        mClazz   = clazz;
+        mIndex   = new DefaultIndex(mFactory, shape.clone());
+        mIsDirty = false;
+        mFactory = factory;
+    }
+
+    protected DefaultArray( final String factory, final Object array ) throws InvalidArrayTypeException {
+        if( array == null ) {
+            throw new InvalidArrayTypeException("Null backing storage is not permitted!");
+        }
+
+        // Check the given array is a real java array
+        Class<?> clazz = array.getClass();
+        while( clazz != null && clazz.isArray() ) {
+            clazz = clazz.getComponentType();
+        }
+        mClazz   = clazz;
+        mIndex   = new DefaultIndex(mFactory, ArrayTools.detectShape( array ) );
+        mFactory = factory;
+        mLock    = false;
+        mIsDirty = false;
+    }
+
+    protected DefaultArray( final DefaultArray array ) {
+        mIndex   = (DefaultIndex) array.mIndex.clone();
+        mIsDirty = array.mIsDirty;
+        mFactory = array.mFactory;
+        mClazz   = array.mClazz;
+        mLock    = array.mLock;
+    }
+
+
+
+    @Override
+    public IArray copy() {
+        return copy(true);
+    }
+
+    @Override
+    public void lock() {
+        mLock = true;
+    }
+
+    @Override
+    public void unlock() {
+        mLock = false;
+    }
+
+    public boolean isLocked() {
+        return mLock;
+    }
+
+    @Override
+    public void setIndex(final IIndex index) {
+        if( index instanceof DefaultIndex ) {
+            mIndex = (DefaultIndex) index;
+        }
+        else {
+            mIndex = new DefaultIndex(index);
+        }
+    }
+
+    @Override
+    public String getFactoryName() {
+        return mFactory;
+    }
+
+    @Override
+    public String shapeToString() {
+        int[] shape = getShape();
+        StringBuilder sb = new StringBuilder();
+        if (shape.length != 0) {
+            sb.append('(');
+            for (int i = 0; i < shape.length; i++) {
+                int s = shape[i];
+                if (i > 0) {
+                    sb.append(",");
+                }
+                sb.append(s);
+            }
+            sb.append(')');
+        }
+        return sb.toString();
+    }
+
+    // IArray data manipulation
+    @Override
+    public DefaultIndex getIndex() {
+        return mIndex;
+    }
+
+    @Override
+    public IArrayIterator getIterator() {
+        return new DefaultArrayIterator(this);
+    }
+
+    @Override
+    public int getRank() {
+        return mIndex.getRank();
+    }
+
+    @Override
+    public IArrayIterator getRegionIterator(final int[] reference, final int[] range) throws InvalidRangeException {
+        IArrayIterator iterator = null;
+        IIndex index = mIndex.clone();
+        index.setShape(range);
+        index.setOrigin(reference);
+        iterator = new DefaultArrayIterator(this, index);
+
+        return iterator;
+    }
+
+    @Override
+    public long getRegisterId() {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public long getSize() {
+        return mIndex.getSize();
+    }
+
+    @Override
+    public DefaultSliceIterator getSliceIterator(final int rank) throws ShapeNotMatchException, InvalidRangeException {
+        return new DefaultSliceIterator(this, rank);
+    }
+
+    @Override
+    public IArrayUtils getArrayUtils() {
+        return new DefaultArrayUtils(this);
+    }
+
+
+    @Override
+    public IArrayMath getArrayMath() {
+        return new DefaultCompositeArrayMath(this, Factory.getFactory(mFactory));
+    }
+
+    @Override
+    public int[] getShape() {
+        return mIndex.getShape();
+    }
+
+    @Override
+    public Class<?> getElementType() {
+        return mClazz;
+    }
+
+    @Override
+    public void setDirty(final boolean dirty) {
+        mIsDirty = dirty;
+    }
+
+    @Override
+    public boolean isDirty() {
+        return mIsDirty;
+    }
+
+    /**
+     * Override this method in case of specific need (use of SoftReference for instance).
+     * It is called each time the memory is accessed when (the array isn't locked).
+     * 
+     * The storage of this array, will be replaced by the object returned by this method.
+     * 
+     * @return the backing storage (as it is) of the array
+     * @note override this method to prepare the data
+     */
+    protected abstract Object loadData();
+
+    /**
+     * Returns a direct access on the underlying data.
+     * 
+     * @return the backing storage of the array (as it is)
+     * @note override this method to prepare the data
+     */
+    protected abstract Object getData();
+
+    /**
+     * Override this method in case of specific need (use of SoftReference for instance).
+     * It is called each time the memory is accessed when (the array isn't locked)
+     * 
+     * @return the backing storage of the array
+     */
+    protected abstract Object copyTo1DJavaArray();
+
+    /**
+     * Override this method in case of specific need (use of SoftReference for instance).
+     * It is called each time the memory is accessed when (the array isn't locked)
+     * 
+     * @return the backing storage of the array
+     */
+    protected abstract Object copyToNDJavaArray();
+}
